ee1a5db7092904abae259bd7087f361b7b76e3dc
Egor Kochetkov <pirs2132@gmail.com>
2024-10-02 00:38:35 +0400

    add comments

--- msdt-1/python/src/prices.py
+++ msdt-1/python/src/prices.py
@@ -20,6 +20,7 @@ def prices():
     res = {}
     global connection
     if connection is None:
+        # if connection is not set
         connection = create_lift_pass_db_connection(connection_options)
 
     if request.method == 'PUT':
@@ -44,21 +45,29 @@ def prices():
             age = None
 
         if age and age < 6:
+            # free for children under 6
              res["cost"] = 0
         else:
             if "type" in request.args and request.args["type"] != "night":
+                # apply non night pass reduction
                 apply_reduction(res, request.args, result)
             else:
+                # apply night pass reduction
                 handle_night_pass(res, request.args, result)
     return res
 
 def apply_reduction(res, args, result):
+    """
+    Apply the reduction to the cost of the lift pass
+    The result is placed in the res variable
+    """
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM holidays')
     is_holiday = check_holiday(cursor, args.get("date"))
     reduction = 0
     
     if not is_holiday and "date" in args and datetime.fromisoformat(args["date"]).weekday() == 0:
+        # monday non holiday reduction
         reduction = 35
     
     age = args.get("age", type=int)
@@ -72,6 +81,9 @@ def apply_reduction(res, args, result):
 
 
 def check_holiday(cursor, date_str):
+    """
+    Check if the date is a holiday
+    """
     date = datetime.fromisoformat(date_str)
     for row in cursor.fetchall():
         holiday = row[0]
@@ -81,11 +93,17 @@ def check_holiday(cursor, date_str):
 
 
 def handle_night_pass(res, args, result):
+    """
+    Handle the night pass.
+    Places cost in the result variable if no reduction is applied
+    Or applies a reduction and places the cost to the res
+    """
     age = args.get("age", type=int)
     if age >= 6:
         if age > 64:
             res["cost"] = math.ceil(result["cost"] * 0.4)
         else:
+            # if no reduction is applied
             res.update(result)
     else:
         res["cost"] = 0
