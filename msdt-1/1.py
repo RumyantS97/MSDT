6c5f75db7a0fe8c2b86faa8b42b80bad4e720b12
a.frolov@raccoonsoft.dev <a.frolov@raccoonsoft.dev>
2024-10-06 22:26:34 +0400

    using CamelCase structure

--- msdt-1/1.py
+++ msdt-1/1.py
@@ -1,12 +1,12 @@
 import random
 import math
 
-class node:
+class Node:
     def __init__ (self, data):
         self.data=data
         self.next=None
 
-class linkedlist:
+class LinkedList:
         def __init__(self):
             self.head=None
             
