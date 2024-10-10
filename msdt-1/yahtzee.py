20158514ac94c1e2983763a31da804d32d0e4345
whymeet <koboc777.mail@gmail.com>
2024-10-10 19:17:10 +0400

    In yahtzee.py: method chance. Use *dice instead of a set of variables

--- msdt-1/yahtzee.py
+++ msdt-1/yahtzee.py
@@ -1,14 +1,8 @@
 class Yahtzee:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(*dice):
+        return sum(dice)
 
     @staticmethod
     def yahtzee(dice):
