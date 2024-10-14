34e403d662cd1a7fbe87835a81e006fe9cd5632a
Nnimil <laimenkas@gmail.com>
2024-10-14 12:53:34 +0400

    Reduce cyclomatic complexity

--- msdt-1/tennis.py
+++ msdt-1/tennis.py
@@ -16,44 +16,24 @@ class TennisGameDefactored1:
             self.p2_points += 1
 
     def score(self):
-        result = ""
-        temp_score = 0
-
         if self.p1_points == self.p2_points:
-            result = {
+            return {
                 0: "Love-All",
                 1: "Fifteen-All",
                 2: "Thirty-All",
                 3: "Forty-All",
             }.get(self.p1_points, "Deuce")
 
-        elif self.p1_points >= 4 or self.p2_points >= 4:
+        if self.p1_points >= 4 or self.p2_points >= 4:
             minus_result = self.p1_points - self.p2_points
+            if abs(minus_result) == 1:
+                return f"Advantage {self.player1_name if minus_result > 0 else self.player2_name}"
+            return f"Win for {self.player1_name if minus_result > 0 else self.player2_name}"
 
-            if minus_result == 1:
-                result = "Advantage " + self.player1_name
-            elif minus_result == -1:
-                result = "Advantage " + self.player2_name
-            elif minus_result >= 2:
-                result = "Win for " + self.player1_name
-            else:
-                result = "Win for " + self.player2_name
-
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.p1_points
-                else:
-                    result += "-"
-                    temp_score = self.p2_points
-
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[temp_score]
-
+        result = "-".join([
+            {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}[self.p1_points],
+            {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}[self.p2_points]
+        ])
         return result
 
 
