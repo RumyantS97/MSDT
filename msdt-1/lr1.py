333c3cb30c641959999faf94890146fbdc2f755a
allinuss <147433644+allinuss@users.noreply.github.com>
2024-10-15 17:12:25 +0400

    правила обозначения переменных и названий функций
    вместо CamelCase использован snake_case

--- msdt-1/lr1.py
+++ msdt-1/lr1.py
@@ -2,7 +2,7 @@ import random
 import os.path
 from collections import defaultdict
 
-def info():
+def info_lr2():
     print('Лабораторная работа №2')
     print('Вариант №8, выполнила студентка группы 6104-020302D, Добина Алина')
     print('Задание:')
@@ -15,67 +15,65 @@ def info():
 
 
 # Ввод элементов списка
-def inputList(m):
-    m = list(map(int, input().split()))
-    return (m)
+def input_list():
+    elements = list(map(int, input("Введите элементы списка через пробел: ").split()))
+    return elements
 
 
 # Формирование списка случайных элементов
-def randomList(m, n, a, b):
-    for i in range(n):
-        m.append(random.randint(a, b))
-    return (m)
+def random_list(random_numbers, count, lower_bound, upper_bound):
+    for _ in range(count):
+        random_numbers.append(random.randint(lower_bound, upper_bound))
+    return random_numbers
 
 
 # Поиск максимального нечетного элемента
-def findMax(m):
-    maxim = -10 ** 10
-    for i in range(len(m)):
-        if (m[i] % 2 != 0) and (m[i] > maxim):
-            maxim = m[i]
-    return maxim
+def find_max(odd_numbers):
+    max_odd = -10 ** 10
+    for index in range(len(odd_numbers)):
+        if (odd_numbers[index] % 2 != 0) and (odd_numbers[index] > max_odd):
+            max_odd = odd_numbers[index]
+    return max_odd
 
 
 # Поиск индекса первого двузначного элемента, кратного заданному числу
-def firstelement(m, kr):
-    ind = 0
-    while ind < len(m) and (m[ind] % kr != 0 or m[ind] <= 9 or m[ind] >= 100):
-        ind += 1
-    return ind
+def find_first_element(numbers, divisor):
+    index = 0
+    while index < len(numbers) and (numbers[index] % divisor != 0 or numbers[index] <= 9 or numbers[index] >= 100):
+        index += 1
+    return index
 
 
-# Поиск старшей цифры
-def oldnum(n):
-    n = abs(n)
-    while n > 9:
-        n //= 10
-    return n
+def find_leading_digit(number):
+    number = abs(number)
+    while number > 9:
+        number //= 10
+    return number
 
 
 # Быстрая сортировка по убыванию
-
-def QuickSort(m, left, right):
+def quick_sort(array, left, right):
     ind_left = left
     ind_right = right
     ind_middle = (left + right) // 2
-    middle = m[ind_middle]
+    middle = array[ind_middle]
     while ind_left <= ind_right:
         while oldnum(m[ind_left]) > oldnum(middle):
             ind_left += 1
-        while oldnum(m[ind_right]) < oldnum(middle):
+        while oldnum(array[ind_right]) < oldnum(middle):
             ind_right -= 1
         if ind_left <= ind_right:
-            m[ind_left], m[ind_right] = m[ind_right], m[ind_left]
+            array[ind_left], array[ind_right] = array[ind_right], array[ind_left]
             ind_left += 1
             ind_right -= 1
     if ind_left < right:
-        QuickSort(m, ind_left, right)
+        quick_sort(array, ind_left, right)
     if ind_right > left:
-        QuickSort(m, left, ind_right)
-    return m
+        quick_sort(array, left, ind_right)
+    return array
 
 
-info()
+info_lr2()
 print("Введите способ заполнения списка:")
 print("1 - ввод элементов списка в одну строку через пробел:")
 print("любое число - автоматическая генерация списка из n случайных элементов "
@@ -116,7 +114,7 @@ print(QuickSort(m, 0, len(m) - 1))
 
 
 
-def info():
+def info_lr3():
     print('Лабораторная работа №3')
     print('Вариант №8, выполнила студентка группы 6104-020302D, Добина Алина')
     print()
@@ -138,54 +136,56 @@ def info():
 
 
 # Функция считает кол-во использования букв
-def CounterOfUsingLetters(s):
-    m = [0] * 26
-    ans = ''
-    for i in range(len(s)):
-        for j in range(26):
-            if s[i] == alf[j]:
-                m[j] += 1
-    for k in range(26):
-        if m[k] != 0:
-            ans += alf[k]
-    if s == '\n':
+def count_used_letters(string):
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    letter_counts = [0] * 26
+    result = ''
+    for char in range(len(s)):
+        for index in range(26):
+            if string[i] == alphabet[j]:
+                letter_counts[j] += 1
+    for index in range(26):
+        if letter_counts[k] != 0:
+            result += alphabet[index]
+    if string == '\n':
         return ''
-    if ans != '':
-        return ans
+    if result != '':
+        return result
     else:
         return "В данной строке нет букв"
 
 
 # Функция возвращает строку для результирующего файла
-def GetWord(s):
-    if CounterOfUsingLetters(s) != "В данной строке нет букв":
-        if len(CounterOfUsingLetters(s)) == 26:
+def get_word(string):
+    letter_counts = count_used_letters(string)
+    if letter_counts != "В данной строке нет букв":
+        if len(letter_counts) == 26:
             return 'no'
         else:
-            return CounterOfUsingLetters(s)
+            return letter_counts
     else:
-        return CounterOfUsingLetters(s)
+        return letter_counts
 
 
 # Функция работает с файлами
-def FileToFile(file1, file2):
-    with open(fname1, 'r') as file1, open(fname2, 'w') as file2:
-        for line in file1.readlines():
+def file_to_file(input_file, output_file):
+    with open(input_file, 'r') as infile, open(outpu_file, 'w') as outfile:
+        for line in infile.readlines():
             line = line.upper()
-            file2.write(GetWord(line) + '\n')
+            output_file.write(get_word(line) + '\n')
 
 
-info()
-alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-fname1 = input('Введите имя исходного файла: ')
-if os.path.exists(fname1):
-    fname2 = input("Введите имя результирующего файла: ")
-    FileToFile(fname1, fname2)
+info_lr3()
+
+source_filename = input('Введите имя исходного файла: ')
+if os.path.exists(source_filename):
+    result_filename: str = input("Введите имя результирующего файла: ")
+    file_to_file(source_filename, result_filename)
     print('Задание выполнено')
 else:
     print('Такого файла не существует')
 
-def info():
+def info_lr4():
     print('Лабораторная работа №4')
     print('Вариант №8, выполнила студентка группы 6104-020302D, Добина Алина')
     print()
@@ -215,185 +215,185 @@ def info():
 
 
     # Задание 1
-    def MinN31(n):
+    def min_n_31(n):
         while f(n) != 31:
             n += 1
         return n
 
 
     # Задание 2
-    def Task2(s):
-        if s == 0:
+    def count_multiples_of_3_even_digits(num):
+        if num == 0:
             return 0
-        elif s % 10 == 6 or s % 10 == 0:
-            return Task2(s // 10) + 1
+        elif num % 10 == 6 or num % 10 == 0:
+            return count_multiples_of_3_even_digits(num // 10) + 1
         else:
-            return Task2(s // 10)
+            return count_multiples_of_3_even_digits(num // 10)
 
 
-    info()
-    print('Минимальное значение n, для которого F(n) равно 31 = ', MinN31(1))
+    info_lr4()
+    print('Минимальное значение n, для которого F(n) равно 31 = ', min_n_31(1))
     x = int(input('Введите значение x: '))
-    s = f(x)
-    print('Количество кратных 3 четных цифр результата вычисления F(x) = ', s, ':', Task2(s))
+    result = f(x)
+    print('Количество кратных 3 четных цифр результата вычисления F(x) = ', result, ':', count_multiples_of_3_even_digits(s))
 
 
 class HuffmanNode:
-    def __init__(self, ch, frequency, left, right):
-        self.ch = ch
+    def __init__(self, character, frequency, left=None, right=None):
+        self.character = character
         self.frequency = frequency
         self.left = left
         self.right = right
 
     def __str__(self):
-        return "(" + str(self.ch) + ", " + str(self.frequency) + ")"
+        return f"({self.character}, {self.frequency})"
 
 
 class HuffmanCoding:
-    def getCode(self, input):
-        freqMap = self.buildFrequencyMap(input)
-        nodeQueue = self.sortByFrequence(freqMap)
-        self.root = self.buildTree(nodeQueue)
-        codeMap = self.createHuffmanCode(self.root)
-        return codeMap
+    def get_code(self, input_data):
+        frequency_map = self.build_frequency_map(input_data)
+        node_queue = self.sort_by_frequency(frequency_map)
+        self.root = self.build_tree(node_queue)
+        code_map = self.create_huffman_code(self.root)
+        return code_map
 
-    def buildFrequencyMap(self, input):
-        map = {}
-        for c in input:
-            map[c] = map.get(c, 0) + 1
-        return map
+    def build_frequency_map(self, input_data):
+        frequency_map = {}
+        for character in input_data:
+            frequency_map[character] = frequency_map.get(character, 0) + 1
+        return frequency_map
 
-    def sortByFrequence(self, map):
-        queue = []
-        for k, v in map.items():
-            queue.append(HuffmanNode(k, v, None, None))
-        queue.sort(key=lambda x: x.frequency)
+    def sort_by_frequency(self, frequency_map):
+        queue = [HuffmanNode(char, freq) for char, freq in frequency_map.items()]
+        queue.sort(key=lambda node: node.frequency)
         return queue
 
-    def buildTree(self, nodeQueue):
-        while len(nodeQueue) > 1:
-            node1 = nodeQueue.pop(0)
-            node2 = nodeQueue.pop(0)
-            node = HuffmanNode('', node1.frequency + node2.frequency, node1, node2)
-            nodeQueue.append(node)
-        return nodeQueue.pop(0)
+    def build_tree(self, node_queue):
+        while len(node_queue) > 1:
+            node1 = node_queue.pop(0)
+            node2 = node_queue.pop(0)
+            combined_node = HuffmanNode('', node1.frequency + node2.frequency, node1, node2)
+            node_queue.append(combined_node)
+            node_queue.sort(key=lambda node: node.frequency)  # Maintain order after adding new node
+        return node_queue.pop(0)
 
-    def createHuffmanCode(self, node):
-        map = {}
-        self.createCodeRec(node, map, "")
-        return map
+    def create_huffman_code(self, node):
+        code_map = {}
+        self.create_code_rec(node, code_map, "")
+        return code_map
 
-    def createCodeRec(self, node, map, s):
-        if node.left == None and node.right == None:
-            map[node.ch] = s
+    def create_code_rec(self, node, code_map, current_code):
+        if node.left is None and node.right is None:
+            code_map[node.character] = current_code
             return
-        self.createCodeRec(node.left, map, s + '0')
-        self.createCodeRec(node.right, map, s + '1')
+        self.create_code_rec(node.left, code_map, current_code + '0')
+        self.create_code_rec(node.right, code_map, current_code + '1')
 
-    def encode(self, codeMap, input):
-        s = ""
-        for i in range(0, len(input)):
-            s += codeMap.get(input[i])
-        return s
+    def encode(self, code_map, input_data):
+        return ''.join(code_map[char] for char in input_data)
+
 
     def decode(self, coded):
-        s = ""
-        curr = self.root
-        for i in range(0, len(coded)):
-            curr = curr.right if coded[i] == '1' else curr.left
-            if curr.left == None and curr.right == None:
-                s += curr.ch
-                curr = self.root
-        return s
-
-
-
-
+        decoded_string = ""
+        current_node = self.root
+        for bit in coded:
+            current_node = current_node.right if bit == '1' else current_node.left
+            if current_node.left is None and current_node.right is None:
+                decoded_string += current_node.character
+                current_node = self.root
+        return decoded_string
 
 def naive_search(text, pattern):
-    n = len(text)
-    m = len(pattern)
+    text_length = len(text)
+    pattern_length = len(pattern)
 
-    for i in range(n - m + 1):
-        j = 0
-        while j < m and text[i + j] == pattern[j]:
-            j += 1
-        if j == m:
-            return i
+    for start_index in range(text_length - pattern_length + 1):
+        match_index = 0
+        while match_index < pattern_length and text[start_index + match_index] == pattern[match_index]:
+            match_index += 1
+        if match_index == pattern_length:
+            return start_index  # Возвращает индекс начала совпадения
     return -1
 
 
-def cmp(text, pattern):
-    t = pattern
-    p = [0] * len(t)
-    j = 0
-    i = 1
-    while i < len(t):
-        if t[j] == t[i]:
-            p[i] = j + 1
-            i += 1
-            j += 1
+def kmp_search(text, pattern):
+    # Построение массива префиксов
+    prefix_array = [0] * len(pattern)
+    pattern_index = 0
+    text_index = 1
+
+    while text_index < len(pattern):
+        if pattern[pattern_index] == pattern[text_index]:
+            prefix_array[text_index] = pattern_index + 1
+            text_index += 1
+            pattern_index += 1
         else:
-            if j == 0:
-                p[i] = 0
-                i += 1
+            if pattern_index == 0:
+                prefix_array[text_index] = 0
+                text_index += 1
             else:
-                j = p[j - 1]
-    a = text
-    m = len(t)
-    n = len(a)
-    i = 0
-    j = 0
-    while i < n:
-        if a[i] == t[j]:
-            i += 1
-            j += 1
-            if j == m:
-                print("Подстрока найдена! (кмп)")
+                pattern_index = prefix_array[pattern_index - 1]
+
+    # Поиск подстроки в тексте
+    pattern_length = len(pattern)
+    text_length = len(text)
+    text_index = 0
+    pattern_index = 0
+
+    while text_index < text_length:
+        if text[text_index] == pattern[pattern_index]:
+            text_index += 1
+            pattern_index += 1
+            if pattern_index == pattern_length:
+                print("Подстрока найдена! (КМП)")
                 break
         else:
-            if j > 0:
-                j = p[j - 1]
+            if pattern_index > 0:
+                pattern_index = prefix_array[pattern_index - 1]
             else:
-                i += 1
-    if i == n and j != m:
-        print("Подстрока не найдена (кмп)")
+                text_index += 1
+
+    if text_index == text_length and pattern_index != pattern_length:
+        print("Подстрока не найдена (КМП)")
 
 
-def expand_from_center(s, left, right):
-    while left >= 0 and right < len(s) and s[left] == s[right]:
-        left -= 1
-        right += 1
-    return s[left + 1:right]
+def expand_from_center(string, left_index, right_index):
+    while left_index >= 0 and right_index < len(string) and string[left_index] == string[right_index]:
+        left_index -= 1
+        right_index += 1
+    return string[left_index + 1:right_index]
 
 
-def find_palindromes(s):
-    palindromes = []
-    for i in range(len(s)):
-        pal1 = expand_from_center(s, i, i)
-        if pal1 and len(pal1) != 1:
-            palindromes.append(pal1)
-        pal2 = expand_from_center(s, i, i + 1)
-        if (pal2 and len(pal2) != 1):
-            palindromes.append(pal2)
-    return palindromes
+def find_palindromes(input_string):
+    found_palindromes = []
+    for index in range(len(input_string)):
+        odd_palindrome = expand_from_center(input_string, index, index)
+        if odd_palindrome and len(odd_palindrome) != 1:
+            found_palindromes.append(odd_palindrome)
+        even_palindrome = expand_from_center(input_string, index, index + 1)
+        if even_palindrome and len(even_palindrome) != 1:
+            found_palindromes.append(even_palindrome)
+    return found_palindromes
 
 
 def has_cycle(graph):
-    visited = set()
-    stack = set()
+    visited_nodes = set()
+    recursion_stack = set()
 
-    def dfs(node):
-        if node in stack:
+    def dfs(current_node):
+        if current_node in recursion_stack:
             return True
-        if node in visited:
+        if current_node in visited_nodes:
             return False
-        visited.add(node)
-        stack.add(node)
-        for neighbor in graph[node]:
+
+        visited_nodes.add(current_node)
+        recursion_stack.add(current_node)
+
+        for neighbor in graph[current_node]:
             if dfs(neighbor):
                 return True
-        stack.remove(node)
+
+        recursion_stack.remove(current_node)
         return False
 
     for node in graph:
@@ -406,21 +406,29 @@ def create_graph():
     graph = {}
     print("Введите вершины графа через пробел (например, A B C):")
     vertices = input().split()
+
     for vertex in vertices:
         graph[vertex] = []
+
     print("Введите ребра графа в формате 'вершина1 вершина2' (например, A B).\n"
           "Для завершения ввода введите '0'.")
+
     while True:
-        edge = input().split()
-        if edge[0] == '0':
+        edge_input = input().split()
+        if edge_input[0] == '0':
             break
-        if len(edge) != 2:
+
+        if len(edge_input) != 2:
             print("Неверный формат ввода! Попробуйте снова.")
             continue
-        if edge[0] not in graph or edge[1] not in graph:
+
+        vertex1, vertex2 = edge_input
+        if vertex1 not in graph or vertex2 not in graph:
             print("Такой вершины нет в графе! Попробуйте снова.")
             continue
-        graph[edge[0]].append(edge[1])
+
+        graph[vertex1].append(vertex2)
+
     return graph
 
 
@@ -434,32 +442,39 @@ def menu():
     )
 
 
-
 flag = True
+
 while flag:
     menu()
     print("Выберите номер задания:")
-    choose = int(input())
-    if (choose == 1):
+    choice = int(input())
+
+    if choice == 1:
         print("Введите строку: ")
-        main_s = str(input())
+        main_string = input()
         print("Введите подстроку для поиска: ")
-        subs = str(input())
-        res1 = naive_search(main_s, subs)
-        cmp(main_s, subs)
-        if (res1 != -1):
+        substring = input()
+
+        result_naive = naive_search(main_string, substring)
+        cmp(main_string, substring)
+
+        if result_naive != -1:
             print("Подстрока найдена (наивно)")
         else:
             print("Такая подстрока не найдена (наивно)")
         print()
-    elif (choose == 2):
+
+    elif choice == 2:
         print("Введите строку: ")
-        s = str(input())
-        ans = find_palindromes(s)
-        print(ans)
-    elif (choose == 3):
+        input_string = input()
+        palindromes = find_palindromes(input_string)
+        print(palindromes)
+
+    elif choice == 3:
         print()
-    elif (choose == 4):
+        # Реализация для задания 3
+
+    elif choice == 4:
         graph = create_graph()
         if has_cycle(graph):
             print("Граф содержит циклы")
