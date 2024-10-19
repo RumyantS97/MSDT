1f6870070a591dda5eedf91a6b64b179a144b85f
whymeet <koboc777.mail@gmail.com>
2024-10-19 20:06:27 +0400

    Убрал лишнее объявление переменных, перед циклами

--- msdt-1/yahtzee.py
+++ msdt-1/yahtzee.py
@@ -66,7 +66,6 @@ class Yahtzee:
 
     def fives(self):
         s = 0
-        i = 0
         for i in range(len(self.dice)):
             if self.dice[i] == 5:
                 s = s + 5
@@ -87,7 +86,6 @@ class Yahtzee:
         counts[d3 - 1] += 1
         counts[d4 - 1] += 1
         counts[d5 - 1] += 1
-        at = 0
         for at in range(6):
             if counts[6 - at - 1] == 2:
                 return (6 - at) * 2
@@ -151,9 +149,7 @@ class Yahtzee:
 
     @staticmethod
     def fullHouse(d1, d2, d3, d4, d5):
-        tallies = []
         _2 = False
-        i = 0
         _2_at = 0
         _3 = False
         _3_at = 0
