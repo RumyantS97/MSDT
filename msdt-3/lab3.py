4b7d340263b5ed3955ed4994d422c59c2fdf7dbb
Petros607 <markaryan.petr@mail.ru>
2024-10-28 00:36:31 +0400

    Third try

--- msdt-3/lab3.py
+++ msdt-3/lab3.py
@@ -23,7 +23,6 @@ def check_row(row: dict) -> bool:
     """
     for key, pattern in PATTERNS.items():
         if not(re.match(pattern, row[key])):
-            print(row[key])
             return False
     return True
 
@@ -42,6 +41,5 @@ def read_my_csv() -> list:
 
 variant = 76
 row_numbers = read_my_csv()
-print(len(row_numbers))
 checksum = calculate_checksum(row_numbers)
 serialize_result(variant, checksum)
