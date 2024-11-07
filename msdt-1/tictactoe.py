8ce8c44b173fff226b09b446528501967b4fe63b
RumyanaGrumeza <hevzyf04@gmail.com>
2024-11-07 13:57:02 +0400

    max line length and alignment fixed

--- msdt-1/tictactoe.py
+++ msdt-1/tictactoe.py
@@ -419,7 +419,7 @@ def matritsa(btn_value, btn_number):
         btn_hooray.configure(text=random.choice(hooray_list))
 
     # Проверка первой диагонали
-    elif pole[0][0] == pole[1][1] == pole[2][2] == 1:  # первая диагональ
+    elif pole[0][0] == pole[1][1] == pole[2][2] == 1:
         btn1.configure(bg='red')
         btn5.configure(bg='red')
         btn9.configure(bg='red')
@@ -460,7 +460,11 @@ def matritsa(btn_value, btn_number):
         btn_quit.grid(row=4, column=1)
         btn_hooray.configure(text=random.choice(hooray_list))
 
-    elif pole[0][0] != 0 and pole[0][1] != 0 and pole[0][2] != 0 and pole[1][0] != 0 and pole[1][1] != 0 and pole[1][2] != 0 and pole[2][0] != 0 and pole[2][1] != 0 and pole[2][2] != 0:
+    elif (pole[0][0] != 0 and pole[0][1] != 0
+          and pole[0][2] != 0 and pole[1][0] != 0
+          and pole[1][1] != 0 and pole[1][2] != 0
+          and pole[2][0] != 0 and pole[2][1] != 0
+          and pole[2][2] != 0):
         btn_itog.configure(text='Ничья')
         btn_again.grid(row=4, column=0)
         btn_quit.grid(row=4, column=1)
@@ -516,26 +520,38 @@ ru.geometry('240x339')
 ru.resizable(0, 0)
 lbl_hello = Label(ru, text = 'Крестики-нолики', font = 'Calibri 14')
 lbl_hello.grid(row = 0, column = 0)
-btn_start = Button(text = 'Начать игру ->', font = 'Calibri 14', command = to_player_1)
+btn_start = Button(text = 'Начать игру ->', font = 'Calibri 14',
+                   command = to_player_1)
 btn_start.grid(row = 1, column = 0, sticky = W)
 
 # Виджеты для функций
-lbl_pl1 = Label(ru, text = 'Игрок КРЕСТИКИ, выберите цвет:', font = 'Calibri 12')
-btn_colour_11 = Button(ru, text = '', width = 7, height = 3, bg = 'magenta', command = choice1)
-btn_colour_12 = Button(ru, text = '', width = 7, height = 3, bg = 'magenta', command = choice2)
-btn_colour_13 = Button(ru, text = '', width = 7, height = 3, bg = 'magenta', command = choice3)
+lbl_pl1 = Label(ru, text = 'Игрок КРЕСТИКИ, выберите цвет:',
+                font = 'Calibri 12')
+btn_colour_11 = Button(ru, text = '', width = 7, height = 3,
+                       bg = 'magenta', command = choice1)
+btn_colour_12 = Button(ru, text = '', width = 7, height = 3,
+                       bg = 'magenta', command = choice2)
+btn_colour_13 = Button(ru, text = '', width = 7, height = 3,
+                       bg = 'magenta', command = choice3)
 
-btn_colour_21 = Button(ru, text = '', width = 7, height = 3, bg = 'magenta', command = choice4)
-btn_colour_22 = Button(ru, text = '', width = 7, height = 3, bg = 'magenta', command = choice5)
-btn_colour_23 = Button(ru, text = '', width = 7, height = 3, bg = 'magenta', command = choice6)
-
+btn_colour_21 = Button(ru, text = '', width = 7, height = 3,
+                       bg = 'magenta', command = choice4)
+btn_colour_22 = Button(ru, text = '', width = 7, height = 3,
+                       bg = 'magenta', command = choice5)
+btn_colour_23 = Button(ru, text = '', width = 7, height = 3,
+                       bg = 'magenta', command = choice6)
+###############################################################################
 lbl_ft = Label(ru, text = 'Кто ходит первым?', font = 'Calibri 12')
-btn_kr = Button(ru, text = 'X', font = 'Calibri 12', command = first_turn_kr)
-btn_rand = Button(ru, text = 'random', font = 'Calibri 12', command = first_turn_random)
-btn_nol = Button(ru, text = 'O', font = 'Calibri 12', command = first_turn_nol)
+btn_kr = Button(ru, text = 'X', font = 'Calibri 12',
+                command = first_turn_kr)
+btn_rand = Button(ru, text = 'random', font = 'Calibri 12',
+                  command = first_turn_random)
+btn_nol = Button(ru, text = 'O', font = 'Calibri 12',
+                 command = first_turn_nol)
 
 lbl_turn = Label(ru, text = 'Первый игрок -', font = 'Calibri 12')
-btn_ok_choice = Button(ru, text = 'ОК', font = 'Calibri 12', command = to_game)
+btn_ok_choice = Button(ru, text = 'ОК', font = 'Calibri 12',
+                       command = to_game)
 
 btn1 = Button(text = 'Поставить', width = 10, height = 5, command = stand1)
 btn2 = Button(text = 'Поставить', width = 10, height = 5, command = stand2)
@@ -546,15 +562,19 @@ btn6 = Button(text = 'Поставить', width = 10, height = 5, command = sta
 btn7 = Button(text = 'Поставить', width = 10, height = 5, command = stand7)
 btn8 = Button(text = 'Поставить', width = 10, height = 5, command = stand8)
 btn9 = Button(text = 'Поставить', width = 10, height = 5, command = stand9)
-
+###############################################################################
 btn_verdict = Button(text = 'Победа:', width = 10, height = 2)
 btn_itog = Button(text = '', width = 10, height = 2)
 btn_hooray = Button(text = '', width = 10, height = 2)
-btn_again = Button(ru, text = 'Играть снова', width = 10, height = 2, command = again)
-btn_quit = Button(ru, text = 'Выйти', width = 10, height = 2, command = quit)
+btn_again = Button(ru, text = 'Играть снова', width = 10, height = 2,
+                   command = again)
+btn_quit = Button(ru, text = 'Выйти', width = 10, height = 2,
+                  command = quit)
 
-btn_stay = Button(text = 'Оставить те же цвета', font = 'Calibri 14', command = first_turn)
-btn_change = Button(text = 'Сменить цвета', font = 'Calibri 14', command = to_player_1)
+btn_stay = Button(text = 'Оставить те же цвета', font = 'Calibri 14',
+                  command = first_turn)
+btn_change = Button(text = 'Сменить цвета', font = 'Calibri 14',
+                    command = to_player_1)
 
 # Переменные для функций
 colour11 = ''
@@ -582,10 +602,96 @@ stand8_value = [2, 1]
 stand9_value = [2, 2]
 
 # Списки цветов и восклицаний для случайного выбора
-colours_list =['blanched almond', 'bisque', 'peach puff', 'lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender','lavender blush', 'misty rose', 'midnight blue', 'navy', 'cornflower blue', 'dark slate blue','slate blue', 'medium slate blue', 'light slate blue', 'medium blue', 'royal blue', 'blue','dodger blue', 'deep sky blue', 'sky blue', 'light sky blue', 'steel blue', 'light steel blue', 'light blue', 'powder blue', 'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise', 'cyan', 'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green', 'dark olive green', 'dark sea green', 'sea green', 'medium sea green', 'light sea green', 'pale green', 'spring green', 'lawn green', 'medium spring green', 'green yellow', 'lime green', 'yellow green', 'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod', 'light goldenrod yellow', 'light yellow', 'yellow', 'gold', 'light goldenrod', 'goldenrod', 'dark goldenrod', 'rosy brown', 'indian red', 'saddle brown', 'sandy brown', 'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange', 'coral', 'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink', 'light pink', 'pale violet red', 'maroon', 'medium violet red', 'violet red', 'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple', 'thistle', 'seashell3', 'seashell4', 'bisque2', 'bisque3', 'bisque4', 'PeachPuff2', 'PeachPuff3', 'PeachPuff4', 'LemonChiffon2', 'LemonChiffon3', 'LemonChiffon4', 'cornsilk2', 'cornsilk3', 'cornsilk4', 'ivory2', 'ivory3', 'ivory4', 'honeydew2', 'honeydew3', 'honeydew4', 'LavenderBlush2', 'LavenderBlush3', 'LavenderBlush4', 'MistyRose2', 'MistyRose3','MistyRose4', 'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2', 'SlateBlue3', 'SlateBlue4', 'RoyalBlue1', 'RoyalBlue2', 'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4', 'DodgerBlue2', 'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2', 'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3', 'DeepSkyBlue4', 'SkyBlue1', 'SkyBlue2', 'SkyBlue3', 'SkyBlue4', 'LightSkyBlue1', 'LightSkyBlue2', 'LightSkyBlue3', 'LightSkyBlue4', 'LightSteelBlue1', 'LightSteelBlue2', 'LightSteelBlue3', 'LightSteelBlue4', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4', 'LightCyan2', 'LightCyan3', 'LightCyan4', 'PaleTurquoise1', 'PaleTurquoise2', 'PaleTurquoise3', 'PaleTurquoise4', 'CadetBlue1', 'CadetBlue2', 'CadetBlue3', 'CadetBlue4', 'turquoise1', 'turquoise2', 'turquoise3', 'turquoise4', 'cyan2', 'cyan3', 'cyan4', 'aquamarine2', 'aquamarine4', 'DarkSeaGreen1', 'DarkSeaGreen2', 'DarkSeaGreen3', 'DarkSeaGreen4', 'SeaGreen1', 'SeaGreen2', 'SeaGreen3', 'PaleGreen1', 'PaleGreen2', 'PaleGreen3', 'PaleGreen4', 'SpringGreen2', 'SpringGreen3', 'SpringGreen4', 'green2', 'green3', 'green4', 'chartreuse2', 'chartreuse3', 'chartreuse4', 'OliveDrab1', 'OliveDrab2', 'OliveDrab4', 'DarkOliveGreen1', 'DarkOliveGreen2', 'DarkOliveGreen3', 'DarkOliveGreen4', 'khaki1', 'khaki2', 'khaki3', 'khaki4', 'LightGoldenrod1', 'LightGoldenrod2', 'LightGoldenrod3', 'LightGoldenrod4', 'LightYellow2', 'LightYellow3', 'LightYellow4', 'yellow2', 'yellow3', 'yellow4', 'gold2', 'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3', 'goldenrod4', 'DarkGoldenrod1', 'DarkGoldenrod2', 'DarkGoldenrod3', 'DarkGoldenrod4', 'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2', 'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3', 'sienna4', 'burlywood1', 'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1', 'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2', 'firebrick3', 'firebrick4', 'brown1', 'salmon1', 'salmon2', 'salmon3', 'salmon4', 'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2', 'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2', 'DarkOrange3', 'DarkOrange4',  'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2', 'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4', 'DeepPink2', 'DeepPink3', 'DeepPink4', 'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4', 'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4', 'PaleVioletRed1',  'PaleVioletRed2', 'PaleVioletRed3', 'PaleVioletRed4', 'maroon1', 'maroon2',  'maroon3', 'maroon4', 'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4', 'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2', 'orchid3', 'orchid4', 'plum1', 'plum2', 'plum3', 'plum4', 'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3', 'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3', 'DarkOrchid4', 'purple1', 'purple2', 'purple3', 'purple4', 'MediumPurple1', 'MediumPurple2', 'MediumPurple3', 'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4']
-random.shuffle(colours_list)
-
+###############################################################################
 hooray_list = ['Ура!', 'Круто!', 'Еее)', 'Молодец!', 'Супер!', 'Вау!']
 random.shuffle(hooray_list)
+
+colours_list =[
+    'blanched almond', 'bisque', 'peach puff', 'lemon chiffon',
+    'mint cream', 'azure', 'alice blue', 'lavender',
+    'lavender blush', 'misty rose', 'midnight blue', 'navy',
+    'cornflower blue', 'dark slate blue','slate blue',
+    'medium slate blue', 'light slate blue', 'medium blue',
+    'royal blue', 'blue','dodger blue', 'deep sky blue',
+    'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
+    'light blue', 'powder blue', 'pale turquoise',
+    'dark turquoise', 'medium turquoise', 'turquoise', 'cyan',
+    'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine',
+    'dark green', 'dark olive green', 'dark sea green',
+    'sea green', 'medium sea green', 'light sea green',
+    'pale green', 'spring green', 'lawn green',
+    'medium spring green', 'green yellow', 'lime green',
+    'yellow green', 'forest green', 'olive drab', 'dark khaki',
+    'khaki', 'pale goldenrod', 'light goldenrod yellow',
+    'light yellow', 'yellow', 'gold', 'light goldenrod',
+    'goldenrod', 'dark goldenrod', 'rosy brown', 'indian red',
+    'saddle brown', 'sandy brown', 'dark salmon', 'salmon',
+    'light salmon', 'orange', 'dark orange', 'coral',
+    'light coral', 'tomato', 'orange red', 'red', 'hot pink',
+    'deep pink', 'pink', 'light pink', 'pale violet red', 'maroon',
+    'medium violet red', 'violet red', 'medium orchid',
+    'dark orchid', 'dark violet', 'blue violet', 'purple',
+    'medium purple', 'thistle', 'seashell3', 'seashell4',
+    'bisque2', 'bisque3', 'bisque4', 'PeachPuff2', 'PeachPuff3',
+    'PeachPuff4', 'LemonChiffon2', 'LemonChiffon3',
+    'LemonChiffon4', 'cornsilk2', 'cornsilk3', 'cornsilk4',
+    'ivory2', 'ivory3', 'ivory4', 'honeydew2', 'honeydew3',
+    'honeydew4', 'LavenderBlush2', 'LavenderBlush3',
+    'LavenderBlush4', 'MistyRose2', 'MistyRose3','MistyRose4',
+    'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2',
+    'SlateBlue3', 'SlateBlue4', 'RoyalBlue1', 'RoyalBlue2',
+    'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4', 'DodgerBlue2',
+    'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2',
+    'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3',
+    'DeepSkyBlue4', 'SkyBlue1', 'SkyBlue2', 'SkyBlue3', 'SkyBlue4',
+    'LightSkyBlue1', 'LightSkyBlue2', 'LightSkyBlue3',
+    'LightSkyBlue4', 'LightSteelBlue1', 'LightSteelBlue2',
+    'LightSteelBlue3', 'LightSteelBlue4', 'LightBlue1',
+    'LightBlue2', 'LightBlue3', 'LightBlue4', 'LightCyan2',
+    'LightCyan3', 'LightCyan4', 'PaleTurquoise1', 'PaleTurquoise2',
+    'PaleTurquoise3', 'PaleTurquoise4', 'CadetBlue1', 'CadetBlue2',
+    'CadetBlue3', 'CadetBlue4', 'turquoise1', 'turquoise2',
+    'turquoise3', 'turquoise4', 'cyan2', 'cyan3', 'cyan4',
+    'aquamarine2', 'aquamarine4', 'DarkSeaGreen1', 'DarkSeaGreen2',
+    'DarkSeaGreen3', 'DarkSeaGreen4', 'SeaGreen1', 'SeaGreen2',
+    'SeaGreen3', 'PaleGreen1', 'PaleGreen2', 'PaleGreen3',
+    'PaleGreen4', 'SpringGreen2', 'SpringGreen3', 'SpringGreen4',
+    'green2', 'green3', 'green4', 'chartreuse2', 'chartreuse3',
+    'chartreuse4', 'OliveDrab1', 'OliveDrab2', 'OliveDrab4',
+    'DarkOliveGreen1', 'DarkOliveGreen2', 'DarkOliveGreen3',
+    'DarkOliveGreen4', 'khaki1', 'khaki2', 'khaki3', 'khaki4',
+    'LightGoldenrod1', 'LightGoldenrod2', 'LightGoldenrod3',
+    'LightGoldenrod4', 'LightYellow2', 'LightYellow3',
+    'LightYellow4', 'yellow2', 'yellow3', 'yellow4', 'gold2',
+    'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3',
+    'goldenrod4', 'DarkGoldenrod1', 'DarkGoldenrod2',
+    'DarkGoldenrod3', 'DarkGoldenrod4', 'RosyBrown1', 'RosyBrown2',
+    'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2',
+    'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3',
+    'sienna4', 'burlywood1', 'burlywood2', 'burlywood3',
+    'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1',
+    'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3',
+    'firebrick1', 'firebrick2', 'firebrick3', 'firebrick4',
+    'brown1', 'salmon1', 'salmon2', 'salmon3', 'salmon4',
+    'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2',
+    'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2',
+    'DarkOrange3', 'DarkOrange4',  'coral1', 'coral2', 'coral3',
+    'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2',
+    'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4',
+    'DeepPink2', 'DeepPink3', 'DeepPink4', 'HotPink1', 'HotPink2',
+    'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
+    'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4',
+    'PaleVioletRed1',  'PaleVioletRed2', 'PaleVioletRed3',
+    'PaleVioletRed4', 'maroon1', 'maroon2',  'maroon3', 'maroon4',
+    'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4',
+    'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2',
+    'orchid3', 'orchid4', 'plum1', 'plum2', 'plum3', 'plum4',
+    'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3',
+    'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3',
+    'DarkOrchid4', 'purple1', 'purple2', 'purple3', 'purple4',
+    'MediumPurple1', 'MediumPurple2', 'MediumPurple3',
+    'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4'
+    ]
+random.shuffle(colours_list)
 
 ru.mainloop()
