0e044585c46b2491030cc7bd74cc9a9a2f611528
nikitash812 <nikitashubin812@mail.ru>
2024-11-24 23:04:18 +0400

    Переименование переменных и методов

--- msdt-1/tennis_test.py
+++ msdt-1/tennis_test.py
@@ -5,7 +5,7 @@ from tennis_unittest import test_cases, play_game
 
 class TestTennis:
 
-    @pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
-    def test_get_score(self, p1Points, p2Points, score, p1Name, p2Name):
-        game = play_game(p1Points, p2Points, p1Name, p2Name)
+    @pytest.mark.parametrize('p1_points p2_points score p1_name p2_name'.split(), test_cases)
+    def test_get_score(self, p1_points, p2_points, score, p1_name, p2_name):
+        game = play_game(p1_points, p2_points, p1_name, p2_name)
         assert score == game.score()
