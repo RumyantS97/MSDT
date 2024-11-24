938400b4b7b016529da9d9edf19cd8a2f376779d
Anton <cart0003@mail.ru>
2024-11-24 06:04:38 +0400

    fix: исправление использования лишних пробелов

--- msdt-1/LR01.py
+++ msdt-1/LR01.py
@@ -32,7 +32,7 @@ class ExtendedSerializer(serializers.Serializer):
     class Meta:
         abstract = True
 
-    def to_representation(self , instance):
+    def to_representation(self, instance):
         data = super().to_representation(instance)
         for key in data:
             if data[key] == '':
@@ -40,7 +40,7 @@ class ExtendedSerializer(serializers.Serializer):
         return data
 
 
-class ExtendedModelSerializer(ExtendedSerializer , serializers.ModelSerializer):
+class ExtendedModelSerializer(ExtendedSerializer, serializers.ModelSerializer):
     class Meta:
         abstract = True
 
@@ -63,8 +63,8 @@ class RecursiveSerializer(serializers.Serializer):
     class Meta:
         abstract = True
 
-    def to_representation(self , value):
-        serializer = self.parent.parent.__class__(value , context=self.context)
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
         return serializer.data
 
 
@@ -80,10 +80,10 @@ class PhotoSerialize(ExtendedModelSerializer):
 
     class Meta:
         model = attr_models.Photo
-        fields = ('image' , 'source')
+        fields = ('image', 'source')
         abstract = True
 
-    def to_representation(self , instance):
+    def to_representation(self, instance):
         data = super().to_representation(instance)
         if self.is_represent_by_format:
             data['image'] = to_representation_by_image_format(instance.image.url)
@@ -120,42 +120,42 @@ class SocialsSerializer(ExtendedSerializer):
         fields = ('vk', 'x_site', 'instagram', 'telegram', 'zen', 'youtube', 'rutube', 'facebook')
         abstract = True
 
-    def validate_vk(self , value):
+    def validate_vk(self, value):
         if value and not value.startswith('https://vk.com/'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://vk.com/')
         return value
 
-    def validate_x_site(self , value):
+    def validate_x_site(self, value):
         if value and not value.startswith('https://x.com') and not value.startswith('https://twitter.com'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://x.com или https://twitter.com')
         return value
 
-    def validate_instagram(self , value):
+    def validate_instagram(self, value):
         if value and not value.startswith('https://instagram.com/'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://instagram.com/')
         return value
 
-    def validate_telegram(self , value):
+    def validate_telegram(self, value):
         if value and not value.startswith('https://t.me/'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://t.me/')
         return value
 
-    def validate_zen(self , value):
+    def validate_zen(self, value):
         if value and not value.startswith('https://dzen.ru/') and value.startswith('https://zen.yandex.ru/'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://dzen.ru или https://zen.yandex.ru/')
         return value
 
-    def validate_youtube(self , value):
+    def validate_youtube(self, value):
         if value and not value.startswith('https://youtube.com/'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://youtube.com/')
         return value
 
-    def validate_rutube(self , value):
+    def validate_rutube(self, value):
         if value and not value.startswith('https://rutube.ru/'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://rutube.ru/')
         return value
 
-    def validate_facebook(self , value):
+    def validate_facebook(self, value):
         if value and not value.startswith('https://facebook.com/'):
             raise serializers.ValidationError('Ссылка должна начинаться с https://facebook.com/')
         return value
@@ -185,13 +185,13 @@ class ContactSerializer(ExtendedSerializer):
         fields = ('contact', 'phone', 'email', 'site')
         abstract = True
 
-    def get_contact(self , instance):
+    def get_contact(self, instance):
         contact = self.get_contact_by_fields(instance)
         if contact:
             return contact
         return instance.vk or instance.x_site or instance.instagram or instance.telegram or instance.zen or instance.youtube or instance.rutube or instance.facebook
 
-    def get_contact_by_fields(self , obj):
+    def get_contact_by_fields(self, obj):
         attrs = ['site', 'email', 'phone']
         for attr in attrs:
             if getattr(obj, attr):
@@ -203,7 +203,7 @@ class ContactSerializer(ExtendedSerializer):
                     return f'tel:{getattr(obj, attr)}'
         return None
 
-    def to_representation(self , instance):
+    def to_representation(self, instance):
         if instance is None:
             return {
                 'contact': None,
@@ -243,18 +243,18 @@ class SerializerWithPhoto(serializers.Serializer):
     is_source = False
     is_format = True
 
-    def get_photo(self , obj):
-        if hasattr(obj , 'photo') and hasattr(obj.photo , 'image') and image_exists(obj.photo.image):
+    def get_photo(self, obj):
+        if hasattr(obj, 'photo') and hasattr(obj.photo, 'image') and image_exists(obj.photo.image):
             source = obj.photo.source if obj.photo.source != '' else None
             url = obj.photo.image.url
             # pdb.set_trace()
-            data = self.photo_to_represent(url , source)
+            data = self.photo_to_represent(url, source)
         else:
             url = get_default_photo_url(self.photo_url)
             data = self.photo_to_represent(url)
         return data
 
-    def photo_to_represent(self , url , source=None) -> dict:
+    def photo_to_represent(self, url, source=None) -> dict:
         data = {}
         if self.is_format:
             data['image'] = to_representation_by_image_format(url)
@@ -272,8 +272,8 @@ class SerializerWithContacts(serializers.Serializer):
     class Meta:
         abstract = True
 
-    def get_contacts(self , obj):
-        if hasattr(obj , 'contacts') or not obj.contacts or obj.contacts.contacts_empty():
+    def get_contacts(self, obj):
+        if hasattr(obj, 'contacts') or not obj.contacts or obj.contacts.contacts_empty():
             return {
                 'contact': None,
                 'phone': None,
@@ -282,8 +282,8 @@ class SerializerWithContacts(serializers.Serializer):
             }
         else:
             return ContactSerializer(obj.contacts).data
-    def get_socials(self , obj):
-        if hasattr(obj , 'contacts') or not obj.contacts or obj.contacts.socials_empty():
+    def get_socials(self, obj):
+        if hasattr(obj, 'contacts') or not obj.contacts or obj.contacts.socials_empty():
             return {
                 'vk': None,
                 'x_site': None,
