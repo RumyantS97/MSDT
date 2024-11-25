f005fd62e531a53103e9f98494a4af889e89f3e9
nikitash812 <nikitashubin812@mail.ru>
2024-11-25 22:25:37 +0400

    Исрпавил

--- msdt-1/tennis_test.py
+++ msdt-1/tennis_test.py
@@ -5,7 +5,7 @@ from tennis_unittest import test_cases, play_game
 
 class TestTennis:
 
-    @pytest.mark.parametrize('p1_points p2_points score p1_name p2_name'.split(), test_cases)
-    def test_get_score(self, p1_points, p2_points, score, p1_name, p2_name):
-        game = play_game(p1_points, p2_points, p1_name, p2_name)
+    @pytest.mark.parametrize('player1_points player2_points score player1_name player2_name'.split(), test_cases)
+    def test_get_score(self, player1_points, player2_points, score, player1_name, player2_name):
+        game = play_game(player1_points, player2_points, player1_name, player2_name)
         assert score == game.score()
