f83c86ed6559665d91f7f24e4852f168b9eb2965
a.frolov@raccoonsoft.dev <a.frolov@raccoonsoft.dev>
2024-11-26 15:49:43 +0400

    rm comm

--- msdt-3/main.py
+++ msdt-3/main.py
@@ -42,38 +42,3 @@ def main(variant: int):
                 invalid_row_numbers.append(row_number - 2)
     
     return invalid_row_numbers
-
-
-# def calculate_checksum(row_numbers: List[int]) -> str:
-#     """
-#     Вычисляет md5 хеш от списка целочисленных значений.
-
-#     :param row_numbers: список номеров строк
-#     :return: md5 хеш строки
-#     """
-#     row_numbers.sort()
-#     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
-
-
-# def serialize_result(variant: int, checksum: str) -> None:
-#     """
-#     Метод для сериализации результатов лабораторной работы.
-#     Записывает номер варианта и контрольную сумму в файл result.json.
-
-#     :param variant: номер варианта
-#     :param checksum: контрольная сумма, вычисленная через calculate_checksum()
-#     """
-#     result = {
-#         "variant": variant,
-#         "checksum": checksum
-#     }
-
-#     with open('result.json', 'w', encoding='utf-8') as f:
-#         json.dump(result, f, ensure_ascii=False, indent=4)
-
-
-# # Основная часть программы
-# VARIANT_NUMBER = 80  # Укажите ваш номер варианта
-# checksum = calculate_checksum(invalid_row_numbers)
-# print(checksum)
-# serialize_result(VARIANT_NUMBER, checksum)
