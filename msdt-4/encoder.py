77472c2691b25d988e351b1065d5c1a83e7e7950
Daytel <159631907+Daytel@users.noreply.github.com>
2024-12-05 16:53:03 +0400

    Adding logging to a project

--- msdt-4/encoder.py
+++ msdt-4/encoder.py
@@ -1,12 +1,18 @@
 import os
+import logging
 
 class FileManager:
 
     # Создания менеджера
-    def __init__( self ):
+    def __init__( self, log_file = "file_manager.log" ):
         
-        # Здесь будет инициализация логирования
-        print("init")
+        # Настройка логирования
+        logging.basicConfig(
+            filename = log_file,
+            level = logging.INFO,
+            format = '%(asctime)s - %(levelname)s - %(message)s'
+        )
+        logging.info("FileManager initialized.")
     
     # Чтение файла
     def Read_File( self, file_path, keyword ):
@@ -15,24 +21,24 @@ class FileManager:
         try:
             with open( file_path, 'r' ) as file:
                 context = file.read()
-
-            print(context)
+            logging.info(f'Read file: {file_path}')
+            
             # Шифрование содержимого
             encrypted_text = self.Vigenere_Encrypt( context, keyword )
-            print(encrypted_text)
+            logging.info(f'The content is encrypted: {file_path}')
+            
             # Возврат текста
             return encrypted_text
 
         # При ошибке ничего не делаем
         except Exception as e:
-            print(f'Error reading file {file_path}: {e}')
+            logging.error(f'Error reading file {file_name}: {e}')
             return None
 
 
     # Шифрование текста
     def Vigenere_Encrypt( self, text, keyword ):
 
-        print(text)
         # Инициализируем стартовые переменные
         encrypted_text = []
         keyword_repeated = []
@@ -70,10 +76,11 @@ class FileManager:
         try:
             with open( file_path, 'w' ) as file:
                 file.write(text)
+            logging.info(f'Written to file: {file_path}')
 
         # В случае ошибки ничего не пишем
         except Exception as e:
-            print(f'Error writing to file {file_path}: {e}')
+            logging.error(f'Error writing to file {file_path}: {e}')
 
     # Удаление исходного файла
     def Delete_File( self, file_path ):
@@ -81,10 +88,11 @@ class FileManager:
         # Попытка удаления
         try:
             os.remove(file_path)
+            logging.info(f'Deleted file {file_path}')
 
         # Ошибка удаления
         except Exception as e:
-            print(f'Error deleting file {file_path}: {e}')
+            logging.error(f'Error deleting file {file_path}: {e}')
 
 # Код использования программы шифрования
 if __name__ == "__main__":
