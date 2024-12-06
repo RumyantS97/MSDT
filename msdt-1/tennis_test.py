07511727f070101961573d9a1d00af74db14ec63
Nu11Object <null.object@vk.com>
2024-12-06 13:13:21 +0400

    refactor: test variable names and long lines

--- msdt-1/tennis_test.py
+++ msdt-1/tennis_test.py
@@ -6,7 +6,20 @@ from tennis_unittest import test_cases, play_game
 
 class TestTennis:
 
-    @pytest.mark.parametrize('p1_points p2_points score p1_mame p2_mame'.split(), test_cases)
-    def test_get_score(self, p1_points, p2_points, score, p1_mame, p2_mame):
-        game = play_game(p1_points, p2_points, p1_mame, p2_mame)
+    @pytest.mark.parametrize(
+        'player1_points player2_points score player1_mame player2_mame'.split(),
+        test_cases
+    )
+    def test_get_score(
+            self,
+            player1_points,
+            player2_points,
+            score,
+            player1_mame,
+            player2_mame):
+        game = play_game(
+            player1_points,
+            player2_points,
+            player1_mame,
+            player2_mame)
         assert score == game.score()
