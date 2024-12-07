be0e68993004fc972a8e2b1dc3c5b01dec833fb6
Nu11Object <null.object@vk.com>
2024-12-07 18:26:32 +0400

    refactor(main): remove unused regex

--- msdt-3/main.py
+++ msdt-3/main.py
@@ -5,25 +5,15 @@ from checksum import calculate_checksum, serialize_result
 # Определяем регулярные выражения для валидации
 validators = {
     "email": r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
-    "telephone": r"^\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}$",
     "http_status_message": r"^\d{3} .+$",
-    "height": r"^[0-2]\.\d{2}$",
     "snils": r"^\d{11}$",
-    "inn": r"^\d{12}$",
     "passport": r"^\d{2} \d{2} \d{6}$",
-    "identifier": r"^\d{2}-\d{2}/\d{2}$",
     "ip_v4": r"^((25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\.){3}(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})$",
-    "occupation": r"^[A-Za-zА-Яа-яЁё\s-]+$",
     "longitude": r"^-?([1-8]?\d(\.\d+)?|90(\.0+)?)$",
-    "latitude": r"^-?(1[0-7]\d(\.\d+)?|180(\.0+)?|[1-9]?\d(\.\d+)?)$",
     "hex_color": r"^#[0-9a-fA-F]{6}$",
-    "blood_type": r"^(A|B|AB|O)[+-]$",
     "isbn": r"^\d{3}-\d{1,5}-\d{1,7}-\d{1,7}-\d{1}$",
-    "issn": r"^\d{4}-\d{4}$",
     "locale_code": r"^[a-z]{2}-[a-z]{2}$",
-    "uuid": r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$",
     "time": r"^\d{2}:\d{2}:\d{2}\.\d{6}$",
-    "date": r"^\d{4}-\d{2}-\d{2}$"
 }
 
 
