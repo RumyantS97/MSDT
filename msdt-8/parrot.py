b1f0f627593c9b9e181cebac8b2c813e4a6ef30a
RumyanaGrumeza <hevzyf04@gmail.com>
2024-12-07 23:31:07 +0400

    methods moved to subclasses

--- msdt-8/parrot.py
+++ msdt-8/parrot.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-
 class ParrotType(Enum):
     EUROPEAN = 1
     AFRICAN = 2
@@ -13,30 +12,6 @@ class Parrot:
         self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
         self._nailed = nailed
-
-    def speed(self):
-        match self._type:
-            case ParrotType.EUROPEAN:
-                return self._base_speed()
-            case ParrotType.AFRICAN:
-                return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
-            case ParrotType.NORWEGIAN_BLUE:
-                return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
-
-    def cry(self):
-        match self._type:
-            case ParrotType.EUROPEAN:
-                return "Sqoork!"
-            case ParrotType.AFRICAN:
-                return "Sqaark!"
-            case ParrotType.NORWEGIAN_BLUE:
-                return "Bzzzzzz" if self._voltage > 0 else "..."
-
-    def _compute_base_speed_for_voltage(self, voltage):
-        return min([24.0, voltage * self._base_speed()])
-
-    def _load_factor(self):
-        return 9.0
 
     def _base_speed(self):
         return 12.0
--- msdt-8/parrot_classes.py
+++ msdt-8/parrot_classes.py
@@ -3,11 +3,11 @@ from parrot import Parrot, ParrotType
 def create_parrot(type_of_parrot, number_of_coconuts, voltage, nailed):
     match type_of_parrot:
         case ParrotType.EUROPEAN:
-            parrot = EuropeanParrot(type_of_parrot, number_of_coconuts, voltage, nailed)
+            parrot = EuropeanParrot(ParrotType.EUROPEAN, number_of_coconuts, voltage, nailed)
         case ParrotType.AFRICAN:
-            parrot = AfricanParrot(type_of_parrot, number_of_coconuts, voltage, nailed)
+            parrot = AfricanParrot(ParrotType.AFRICAN, number_of_coconuts, voltage, nailed)
         case ParrotType.NORWEGIAN_BLUE:
-            parrot = NorwegianBlueParrot(type_of_parrot, number_of_coconuts, voltage, nailed)
+            parrot = NorwegianBlueParrot(ParrotType.NORWEGIAN_BLUE, number_of_coconuts, voltage, nailed)
 
     return parrot
 
@@ -15,10 +15,34 @@ class EuropeanParrot(Parrot):
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
         super().__init__(type_of_parrot, number_of_coconuts, voltage, nailed)
 
+    def speed(self):
+        return self._base_speed()
+
+    def cry(self):
+        return "Sqoork!"
+
 class AfricanParrot(Parrot):
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
         super().__init__(type_of_parrot, number_of_coconuts, voltage, nailed)
 
+    def speed(self):
+        return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
+
+    def cry(self):
+        return "Sqaark!"
+
+    def _load_factor(self):
+        return 9.0
+
 class NorwegianBlueParrot(Parrot):
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
         super().__init__(type_of_parrot, number_of_coconuts, voltage, nailed)
+
+    def speed(self):
+        return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
+
+    def cry(self):
+        return "Bzzzzzz" if self._voltage > 0 else "..."
+
+    def _compute_base_speed_for_voltage(self, voltage):
+        return min([24.0, voltage * self._base_speed()])
