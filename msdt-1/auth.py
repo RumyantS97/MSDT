392cfd6cecd30b075034dd05b945d3a19dd57d50
DaniilRyazan <raiseman.m@gmail.com>
2024-12-12 23:03:04 +0400

    del comments, transfer lines

--- msdt-1/auth.py
+++ msdt-1/auth.py
@@ -3,12 +3,15 @@ import shutil
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+from flask import (Blueprint, render_template,
+                   request, flash, redirect,
+                   url_for, session)
 from flask_login import login_user, logout_user, login_required, current_user
 from models import db, User
 
 
 auth_bp = Blueprint('auth', __name__)
+
 
 @auth_bp.route("/login", methods=['GET', 'POST'])
 def login():
@@ -25,6 +28,7 @@ def login():
             flash('Invalid credentials.', 'error')
     return render_template("login.html")
 
+
 @auth_bp.route("/logout")
 @login_required
 def logout():
@@ -36,6 +40,7 @@ def logout():
     logout_user()
     flash('You have been logged out.', 'info')
     return redirect(url_for('auth.login'))
+
 
 @auth_bp.route("/register", methods=['GET', 'POST'])
 def register():
@@ -52,7 +57,8 @@ def register():
             flash('Username already exists.', 'warning')
             return redirect(url_for('auth.register'))
         
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        hashed_password = generate_password_hash(password,
+                                                 method='pbkdf2:sha256')
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
--- msdt-1/books.py
+++ msdt-1/books.py
@@ -1,7 +1,10 @@
 import os
 
 import ebooklib
-from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app, session, jsonify, send_from_directory
+from flask import (Blueprint, render_template,
+                   request, flash, redirect,
+                    url_for, current_app, session,
+                    jsonify, send_from_directory)
 from flask_login import login_required, current_user
 from ebooklib import epub
 
@@ -22,9 +25,10 @@ def ebook_images(filename):
 @login_required
 def book_details():
     user_books = Book.query.filter_by(user_id=current_user.id).all()
-    # fetch all the books of user and display the all the details of the book
     book_details = [fetch_book_details(book.title) for book in user_books]
-    return render_template("book_details.html", books=user_books, details=book_details)
+    return render_template("book_details.html",
+                            books=user_books,
+                            details=book_details)
     
 
 @books_bp.route("/upload", methods=['GET', 'POST'])
@@ -35,15 +39,15 @@ def upload():
         author = request.form['author']
         file = request.files['file']
         
-        # fetch book details from Google Books API
         book_details = fetch_book_details(title, author)
         thumbnail = book_details.get('thumbnail', 'No thumbnail available')
 
-        # Save the uploaded file
-        file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], file.filename))
-        
-        # Process the uploaded file (e.g., extract metadata, save to database)
-        book = Book(title=title, author=author, file=file.filename, user_id=current_user.id, thumbnail=thumbnail)
+        file.save(os.path.join(current_app.config['UPLOAD_FOLDER'],
+                               file.filename))
+        book = Book(title=title, author=author,
+                    file=file.filename,
+                    user_id=current_user.id,
+                    thumbnail=thumbnail)
         db.session.add(book)
         db.session.commit()
         
@@ -60,17 +64,18 @@ def read(book_id):
     if book is None:
         flash('Book not found.', 'error')
         return redirect(url_for('index'))
-
-    # Get or create the UserBook entry
-    user_book = UserBook.query.filter_by(user_id=current_user.id, book_id=book_id).first()
+    user_book = UserBook.query.filter_by(user_id=current_user.id,
+                                         book_id=book_id).first()
     if user_book is None:
-        user_book = UserBook(user_id=current_user.id, book_id=book_id, last_read_chapter=0)
+        user_book = UserBook(user_id=current_user.id,
+                             book_id=book_id,
+                             last_read_chapter=0)
         db.session.add(user_book)
         db.session.commit()
-
     file = book.file
     session["file"] = file
-    book = epub.read_epub(os.path.join(current_app.config['UPLOAD_FOLDER'], file))
+    book = epub.read_epub(os.path.join(current_app.config['UPLOAD_FOLDER'],
+                                       file))
     chapters = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
     last_read_chapter = user_book.last_read_chapter
     print(f"Last read chapter retrieved from database: {last_read_chapter}")
@@ -88,5 +93,5 @@ def delete_book(book_id):
         db.session.commit()
         flash('Book deleted successfully.', 'success')
         return jsonify({'success': True}), 200
-    return jsonify({'success': False, 'message': 'Book not found or unauthorized'}), 404
-    
+    return jsonify({'success': False,
+                    'message': 'Book not found or unauthorized'}), 404
