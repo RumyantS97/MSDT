762c9880e7857e3eab2ee9b76a1036cfb9e0d6ff
DaniilRyazan <raiseman.m@gmail.com>
2024-12-13 15:49:20 +0400

    read csv and validate rows

--- msdt-3/functions.py
+++ msdt-3/functions.py
@@ -1,10 +1,38 @@
 import json
+import csv
+import re
 
 
 def write_to_json_file(path: str, data: dict) -> None:
     """
-    Writes a dict to json file
+    writes a dict to json file
     """
     with open(path, 'w', encoding='utf-8') as file:
         json.dump(data, file)
     
+
+def read_csv(path: str) -> list[list[str]]:
+    """
+    read csv file
+    """
+    data = []
+    with open(path, "r", encoding="utf-16") as file:
+        file_reader = csv.reader(file, delimiter=';')
+        next(file_reader, None)
+        for row in file_reader:
+            data.append(row)
+        return data
+    
+
+def validate_rows(data: list[list[str]], regular: dict) -> list[int]:
+    """
+    validate rows in a dataset against a set of regular expression patterns
+    """
+    invalid_rows = []
+    for number, row in enumerate(data):
+        for _, (field, key) in enumerate(zip(row, regular.keys())):
+            pattern = regular[key]
+            if not re.fullmatch(pattern, field):
+                invalid_rows.append(number)
+                break
+    return invalid_rows
