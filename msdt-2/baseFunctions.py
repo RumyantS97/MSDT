6aa6cdb95df45ff526bfa0af63dfac3ff9acf21b
Andrey <emeralderm20@gmail.com>
2024-12-14 11:58:40 +0400

    Run ruff formator

--- msdt-2/baseFunctions.py
+++ msdt-2/baseFunctions.py
@@ -3,11 +3,11 @@ import os
 from scipy.signal import spectrogram
 from display import read_file
 
-def get_data_set(dyrectory):
 
+def get_data_set(dyrectory):
     files_name = os.listdir(dyrectory)
     y = []
-    files_names =[]
+    files_names = []
     srs = []
     for file in files_name:
         path = dyrectory + "/" + file
@@ -19,22 +19,41 @@ def get_data_set(dyrectory):
     return y, files_names, srs
 
 
-def audio_to_mel(y,sr):
-    return librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax = 8000)
+def audio_to_mel(y, sr):
+    return librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
 
 
 def define_class(y1, y2):
     dec = 0
-    if (("kitchen" in y1) and ("kitchen" in y2)) and (("mi" in y1) and ("mi" in y2)): dec = 1
-    if (("kitchen" in y1) and ("kitchen" in y2)) and (("android" in y1) and ("android" in y2)): dec = 1
-    if (("kitchen" in y1) and ("kitchen" in y2)) and (("iphone" in y1) and ("iphone" in y2)): dec = 1
-    if (("room" in y1) and ("room" in y2)) and (("mi" in y1) and ("mi" in y2)): dec = 1
-    if (("room" in y1) and ("room" in y2)) and (("android" in y1) and ("android" in y2)): dec = 1
-    if (("room" in y1) and ("room" in y2)) and (("iphone" in y1) and ("iphone" in y2)): dec = 1
-    if (("electro" in y1) and ("electro" in y2)): dec = 1
-    if (("second_pink" in y1) and ("second_pink" in y2)): dec = 1
+    if (("kitchen" in y1) and ("kitchen" in y2)) and (
+        ("mi" in y1) and ("mi" in y2)
+    ):
+        dec = 1
+    if (("kitchen" in y1) and ("kitchen" in y2)) and (
+        ("android" in y1) and ("android" in y2)
+    ):
+        dec = 1
+    if (("kitchen" in y1) and ("kitchen" in y2)) and (
+        ("iphone" in y1) and ("iphone" in y2)
+    ):
+        dec = 1
+    if (("room" in y1) and ("room" in y2)) and (("mi" in y1) and ("mi" in y2)):
+        dec = 1
+    if (("room" in y1) and ("room" in y2)) and (
+        ("android" in y1) and ("android" in y2)
+    ):
+        dec = 1
+    if (("room" in y1) and ("room" in y2)) and (
+        ("iphone" in y1) and ("iphone" in y2)
+    ):
+        dec = 1
+    if ("electro" in y1) and ("electro" in y2):
+        dec = 1
+    if ("second_pink" in y1) and ("second_pink" in y2):
+        dec = 1
 
     return dec
+
 
 def spectrogramm(x):
     f1, t1, s1 = spectrogram(x)
--- msdt-2/statisticsClassification.py
+++ msdt-2/statisticsClassification.py
@@ -1,11 +1,17 @@
 import statistics
 from baseFunctions import get_data_set, define_class
 import numpy as np
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+)
 from sklearn.ensemble import RandomForestClassifier as classifier
 from sklearn.model_selection import train_test_split
 import pandas
-np.seterr(divide='ignore', invalid='ignore')
+
+np.seterr(divide="ignore", invalid="ignore")
 
 data_set, files, srs = get_data_set("dataset")
 
@@ -22,10 +28,12 @@ train_split = data_set[0:]
 
 for i in range(len(train_split)):
     for j in range(i + 1, len(train_split)):
-        [a, b, c] = statistics.use_stat_for_spectr(train_split[i], train_split[j])
+        [a, b, c] = statistics.use_stat_for_spectr(
+            train_split[i], train_split[j]
+        )
         label = define_class(files[i], files[j])
         labels.append(label)
-        middle = (a + b + c)/3
+        middle = (a + b + c) / 3
         middles.append(middle)
         chi_points.append(a)
         umann_points.append(b)
@@ -33,8 +41,16 @@ for i in range(len(train_split)):
         vec = [a, b, c]
         all_vec.append(vec)
 
-df = pandas.DataFrame({'chi': chi_points, 'unamm': umann_points, 'stud': student_points, 'pair': labels, 'middle': middles})
-df.to_csv('stats.csv')
+df = pandas.DataFrame(
+    {
+        "chi": chi_points,
+        "unamm": umann_points,
+        "stud": student_points,
+        "pair": labels,
+        "middle": middles,
+    }
+)
+df.to_csv("stats.csv")
 
 inside_points_chi = []
 inside_points_student = []
@@ -52,22 +68,32 @@ mid_model = classifier()
 all_model = classifier()
 
 chi_points = np.array(chi_points)
-chi_points = chi_points.reshape(-1,1)
+chi_points = chi_points.reshape(-1, 1)
 umann_points = np.array(umann_points)
-umann_points = chi_points.reshape(-1,1)
+umann_points = chi_points.reshape(-1, 1)
 student_points = np.array(student_points)
-student_points = chi_points.reshape(-1,1)
+student_points = chi_points.reshape(-1, 1)
 middles = np.array(middles)
-middles = chi_points.reshape(-1,1)
+middles = chi_points.reshape(-1, 1)
 all_vec = np.array(all_vec)
-all_vec = chi_points.reshape(-1,1)
+all_vec = chi_points.reshape(-1, 1)
 
 
-chi_x_train, chi_x_test, chi_y_train, chi_y_test = train_test_split(chi_points, labels, test_size=0.30)
-umann_x_train, umann_x_test, umann_y_train, umann_y_test = train_test_split(umann_points, labels, test_size=0.30)
-stud_x_train, stud_x_test, stud_y_train, stud_y_test = train_test_split(student_points, labels, test_size=0.30)
-mid_x_train, mid_x_test, mid_y_train, mid_y_test = train_test_split(middles, labels, test_size=0.30)
-all_x_train, all_x_test, all_y_train, all_y_test = train_test_split(all_vec, labels, test_size=0.30)
+chi_x_train, chi_x_test, chi_y_train, chi_y_test = train_test_split(
+    chi_points, labels, test_size=0.30
+)
+umann_x_train, umann_x_test, umann_y_train, umann_y_test = train_test_split(
+    umann_points, labels, test_size=0.30
+)
+stud_x_train, stud_x_test, stud_y_train, stud_y_test = train_test_split(
+    student_points, labels, test_size=0.30
+)
+mid_x_train, mid_x_test, mid_y_train, mid_y_test = train_test_split(
+    middles, labels, test_size=0.30
+)
+all_x_train, all_x_test, all_y_train, all_y_test = train_test_split(
+    all_vec, labels, test_size=0.30
+)
 
 chi_model.fit(chi_x_train, chi_y_train)
 umann_model.fit(umann_x_train, umann_y_train)
@@ -82,11 +108,36 @@ umann_pred = umann_model.predict(umann_x_test)
 middles_pred = mid_model.predict(mid_x_test)
 all_pred = all_model.predict(all_x_test)
 
-report_chi = [accuracy_score(chi_y_test, chi_pred), f1_score(chi_y_test, chi_pred), precision_score(chi_y_test, chi_pred), recall_score(chi_y_test, chi_pred)]
-report_uman = [accuracy_score(umann_y_test, umann_pred), f1_score(umann_y_test, umann_pred), precision_score(umann_y_test, umann_pred), recall_score(umann_y_test, umann_pred)]
-report_stud = [accuracy_score(stud_y_test, stud_pred), f1_score(stud_y_test, stud_pred), precision_score(stud_y_test, stud_pred), recall_score(stud_y_test, stud_pred)]
-report_mid = [accuracy_score(mid_y_test, middles_pred), f1_score(mid_y_test, middles_pred), precision_score(mid_y_test, middles_pred), recall_score(mid_y_test, middles_pred)]
-report_all = [accuracy_score(all_y_test, all_pred), f1_score(all_y_test, all_pred), precision_score(all_y_test, all_pred), recall_score(all_y_test, all_pred)]
+report_chi = [
+    accuracy_score(chi_y_test, chi_pred),
+    f1_score(chi_y_test, chi_pred),
+    precision_score(chi_y_test, chi_pred),
+    recall_score(chi_y_test, chi_pred),
+]
+report_uman = [
+    accuracy_score(umann_y_test, umann_pred),
+    f1_score(umann_y_test, umann_pred),
+    precision_score(umann_y_test, umann_pred),
+    recall_score(umann_y_test, umann_pred),
+]
+report_stud = [
+    accuracy_score(stud_y_test, stud_pred),
+    f1_score(stud_y_test, stud_pred),
+    precision_score(stud_y_test, stud_pred),
+    recall_score(stud_y_test, stud_pred),
+]
+report_mid = [
+    accuracy_score(mid_y_test, middles_pred),
+    f1_score(mid_y_test, middles_pred),
+    precision_score(mid_y_test, middles_pred),
+    recall_score(mid_y_test, middles_pred),
+]
+report_all = [
+    accuracy_score(all_y_test, all_pred),
+    f1_score(all_y_test, all_pred),
+    precision_score(all_y_test, all_pred),
+    recall_score(all_y_test, all_pred),
+]
 
 print("1", report_chi)
 print("2", report_uman)
