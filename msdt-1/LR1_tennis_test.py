e7bbb6f7eaab359b9a49075a3b7771413656f3d0
F4NT0M <evgeniylebedev55@gmail.com>
2024-12-16 20:45:57 +0400

    Добавление кода в LR1_tennis_test

---
+++ msdt-1/LR1_tennis_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from LR1_tennis import TennisGame
+
+from LR1_unittest import test_cases, play_game
+
+class TestTennis:
+
+    @pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
+    def test_get_score(self, p1Points, p2Points, score, p1Name, p2Name):
+        game = play_game(p1Points, p2Points, p1Name, p2Name)
+        assert score == game.score()
