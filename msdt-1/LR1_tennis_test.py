e6da6b496ef47bda3aebc5447e35062cf253fee1
F4NT0M <evgeniylebedev55@gmail.com>
2024-12-16 20:49:01 +0400

    Ограничение длины строк до 79 символов.

--- msdt-1/LR1_tennis_test.py
+++ msdt-1/LR1_tennis_test.py
@@ -4,9 +4,11 @@ from LR1_tennis import TennisGame
 
 from LR1_unittest import test_cases, play_game
 
+
 class TestTennis:
 
-    @pytest.mark.parametrize('p1_points p2_points score p1_name p2_name'.split(), test_cases)
+    @pytest.mark.parametrize(
+        'p1_points p2_points score p1_name p2_name'.split(), test_cases)
     def test_get_score(self, p1_points, p2_points, score, p1_name, p2_name):
         game = play_game(p1_points, p2_points, p1_name, p2_name)
         assert score == game.score()
