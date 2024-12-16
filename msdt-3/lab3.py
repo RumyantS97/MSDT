82eed842009eb932a68b0e927b30828ca2101382
DariaA-a <teoriabigvzr@gmail.com>
2024-12-16 16:14:53 +0400

    lr3 - fixed

--- msdt-3/lab3.py
+++ msdt-3/lab3.py
@@ -3,16 +3,16 @@ import re
 from checksum import calculate_checksum, serialize_result
 
 patterns = {
-    "telephone": r"^\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}$",
-    "height": r"^[12]\.\d{2}$",
-    "snils": r'^\d{11}$',
-    "identifier": r"^\d{2}-\d{2}/\d{2}$",
-    "occupation": r"^[A-Za-zА-Яа-яёЁ\s-]+$",
-    "longitude": r'^(-?(180(\.0{1,6})?|(\d{1,2}|1[0-7]\d)(\.\d{1,6})?))$',
-    "blood_type": r"^(A|B|AB|O)[\u2212\+]{1}$",
-    "issn": r"^\d{4}-\d{4}$",
-    "locale_code": r'^[a-z]{2}(-[a-z]{2})?$',
-    "date": r"^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
+    "telephone"   : r"^\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}$",
+    "height"      : r"^[12]\.\d{2}$",
+    "snils"       : r"^\d{11}$",
+    "identifier"  : r"^\d{2}-\d{2}/\d{2}$",
+    "occupation"  : r"^[A-Za-zА-Яа-яёЁ\s-]+$",
+    "longitude"   : r"^(-?(180(\.0{1,6})?|(\d{1,2}|1[0-7]\d)(\.\d{1,6})?))$",
+    "blood_type"  : r"^(A|B|AB|O)[\u2212\+]{1}$",
+    "issn"        : r"^\d{4}-\d{4}$",
+    "locale_code" : r"^[a-z]{2}(-[a-z]{2})?$",
+    "date"        : r"^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
 }
 
 
