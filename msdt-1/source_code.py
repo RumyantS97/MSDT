4254d9e1c121cc9b53cd1edd5c9740771b17f81c
Militina <milana-zakharova@mail.ru>
2024-12-16 08:50:24 +0400

    Используйте 4 пробела на каждый уровень отступа

--- msdt-1/source_code.py
+++ msdt-1/source_code.py
@@ -1,20 +1,20 @@
 def less_than(a, b):
-  return a < b
+    return a < b
 
 def greater_than(a, b):
-  return a > b
+    return a > b
 
 def equal_to(a, b):
-  return a == b
+    return a == b
 
 def not_equal_to(a, b):
-  return a != b
+    return a != b
 
 def less_than_or_equal_to(a, b):
-  return a <= b
+    return a <= b
 
 def greater_than_or_equal_to(a, b):
-  return a >= b
+    return a >= b
 
 operator_lt = less_than
 operator_gt = greater_than
