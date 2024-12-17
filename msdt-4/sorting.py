267ea7229c2a5d7f54e9980cf1b1df673cc86ea6
Militina <milana-zakharova@mail.ru>
2024-12-17 12:43:44 +0400

    Добавлены файлы логов для примера, изменен базовый уровень логирования на DEBUG, расширение файлов лога изменено на .log чтобы не мусорить в индекс

--- msdt-4/sorting.py
+++ msdt-4/sorting.py
@@ -6,8 +6,8 @@ import logging
 # Новый уровень, т.к. логи о вызовах методов операторов засоряют лог
 logging.addLevelName(5, 'DEBUG_EX')
 logging.basicConfig(
-    filename=f'Log_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.txt',
-    level=logging.ERROR,
+    filename=f'Log_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.log',
+    level=logging.DEBUG,
     format='[%(asctime)s] [%(levelname)s] => %(message)s',
     encoding='utf-8',
     datefmt='%Y-%m-%d %H:%M:%S'
