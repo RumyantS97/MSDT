2493d8f4d21c95d9603619b4a14425acd48b4a8e
Militina <milana-zakharova@mail.ru>
2024-12-17 14:13:14 +0400

    Сохранение файлов логов перенесено в отдельную папку

--- msdt-8/logging_config.py
+++ msdt-8/logging_config.py
@@ -1,11 +1,17 @@
+import os
 import logging
 from datetime import datetime
+
+log_dir = 'logs'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
 
 # Новый уровень, т.к. логи о вызовах методов операторов засоряют лог
 logging.addLevelName(5, 'DEBUG_EX')
 DEBUG_EX = 5
 logging.basicConfig(
-    filename=f'Log_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.log',
+    filename=os.path.join(
+        log_dir, f'Log_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.log'),
     level=logging.DEBUG,
     format='[%(asctime)s] [%(levelname)s] => %(message)s',
     encoding='utf-8',
