a662580d05bf653f89fcf92f7379370f978079cc
Jokira <dasha04cher@gmail.com>
2024-12-19 10:48:20 +0400

    Упрощено ветвление выбора операции, теперь запрашиваются данные основываясь на значении operand_count

--- msdt-8/calc.py
+++ msdt-8/calc.py
@@ -6,45 +6,27 @@ def calculator():
     while True:
         print(OperationsEnum.get_message_for_input())
         choice = input("Введите номер операции: ")
-        num1 = num2 = 0
         if choice in OperationsEnum.get_codes():
-            if choice in ['1', '2', '3', '4', '5']:
+            operation = OperationsEnum.get_item_by_code(choice)
+            if operation.operand_count == 2:
                 try:
                     num1 = float(input("Введите первое число: "))
                     num2 = float(input("Введите второе число: "))
                 except ValueError:
                     print("Ошибка: введите числовое значение.")
                     continue
-
-            if choice == '1':
-                print(f"{num1} + {num2} = {OperationsEnum.ADD.func(num1, num2)}")
-            elif choice == '2':
-                print(f"{num1} - {num2} = {OperationsEnum.SUB.func(num1, num2)}")
-            elif choice == '3':
-                print(f"{num1} * {num2} = {OperationsEnum.MUL.func(num1, num2)}")
-            elif choice == '4':
-                result = OperationsEnum.DIV.func(num1, num2)
-                print(f"{num1} / {num2} = {result}")
-            elif choice == '5':
-                print(f"{num1} ^ {num2} = {OperationsEnum.POW.func(num1, num2)}")
-            elif choice in ['6', '7', '8']:
+                print(f'{operation.description} для чисел {num1} и {num2} = {operation.func(num1, num2)}')
+            elif operation.operand_count == 1:
                 try:
-                    angle = float(input("Введите угол в градусах: "))
+                    num = float(input("Введите число: "))
                 except ValueError:
                     print("Ошибка: введите числовое значение.")
                     continue
-
-                if choice == '6':
-                    print(f"sin({angle}) = {OperationsEnum.SIN.func(angle)}")
-                elif choice == '7':
-                    print(f"cos({angle}) = {OperationsEnum.COS.func(angle)}")
-                elif choice == '8':
-                    print(f"tan({angle}) = {OperationsEnum.TAN.func(angle)}")
-
+                print(f'{operation.description} числа {num} = {operation.func(num)}')
         else:
-            print("Некорректный ввод. Пожалуйста, выберите номер операции от 1 до 8.")
+            print("Некорректный ввод. Пожалуйста, выбирайте номер операции из списка.")
 
-        next_calculation = input("Хотите выполнить еще одно вычисление? (да/нет): ")
+        next_calculation = input("Хотите выполнить еще одно вычисление? (да/any key): ")
         if next_calculation.lower() != 'да':
             break
 
