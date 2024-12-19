a586070e4cdbcda7127e5cb062e6347113e73d7e
prilezhnyy-uchenik <Dariys63@mail.ru>
2024-12-19 22:34:18 +0400

    snake_case

--- msdt-1/tennis1.py
+++ msdt-1/tennis1.py
@@ -2,26 +2,26 @@ class TennisGame1:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.p1points = 0
-        self.p2points = 0
+        self.p1_points = 0
+        self.p2_points = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.p1points += 1
+            self.p1_points += 1
         else:
-            self.p2points += 1
+            self.p2_points += 1
 
     def score(self):
         result = ""
         temp_score = 0
-        if self.p1points == self.p2points:
+        if self.p1_points == self.p2_points:
             result = {
                 0: "Love-All",
                 1: "Fifteen-All",
                 2: "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif self.p1points >= 4 or self.p2points >= 4:
-            minus_result = self.p1points - self.p2points
+            }.get(self.p1_points, "Deuce")
+        elif self.p1_points >= 4 or self.p2_points >= 4:
+            minus_result = self.p1_points - self.p2_points
             if minus_result == 1:
                 result = "Advantage player1"
             elif minus_result == -1:
@@ -33,10 +33,10 @@ class TennisGame1:
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.p1points
+                    temp_score = self.p1_points
                 else:
                     result += "-"
-                    temp_score = self.p2points
+                    temp_score = self.p2_points
                 result += {
                     0: "Love",
                     1: "Fifteen",
