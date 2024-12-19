1e5244befbd9177360bf8dc43117e61d0b7ede6c
Jokira <dasha04cher@gmail.com>
2024-12-19 09:03:08 +0400

    Добавлена запись в csv файл и csv файл полученный в результате работы этой программы

--- msdt-3/lab3.py
+++ msdt-3/lab3.py
@@ -1,4 +1,5 @@
 import re
+import csv
 
 
 def find_first_value(string, regex):
@@ -19,11 +20,31 @@ class Product:
     def __init__(self, card):
         self.price = find_first_value(card, r'product-buy__price">(.*?)&')
         self.payment = find_first_value(card, r'product-buy__sub">от (.*?)&')
-        self.rating = find_first_value(card, r'catalog-product__rating.*?b>([\d.]+).*?</b')
-        self.is_sales_stopped = (find_first_value_without_group(card, r'Продажи прекращены') is not None)
+        self.rating = find_first_value(
+            card, r'catalog-product__rating.*?b>([\d.]+).*?</b')
+        self.is_sales_stopped = (find_first_value_without_group(
+            card, r'Продажи прекращены') is not None)
         self.description = find_first_value(card, r'span>(.*?)</span')
-        self.name = find_first_value_without_group(self.description, r'Стиральн.+?машина[a-zA-Z0-9 /-]+')
+        self.name = find_first_value_without_group(
+            self.description, r'Стиральн.+?машина[a-zA-Z0-9 /-]+')
         self.turn = find_first_value(self.description, r'([\d]+?) об/мин')
+
+    def to_dict(self):
+        return {"price": self.price, "payment": self.payment,
+                "rating": self.rating, "name": self.name,
+                "is_sales_stopped": self.is_sales_stopped,
+                "description": self.description, "turn": self.turn}
+
+
+def to_csv_file(file_name, products_):
+    with open(file_name, 'w', newline='', encoding="utf-8") as file:
+        fieldnames = ["name", "price", "payment", "rating", "turn",
+                      "is_sales_stopped", "description"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for product_ in products_:
+            writer.writerow(product_.to_dict())
 
 
 if __name__ == "__main__":
@@ -31,8 +52,11 @@ if __name__ == "__main__":
         page = file.read()
 
     category_name = find_first_value(page, r'Поиск по категории: (.*?)<')
-    product_list = find_first_value(page, r'class="products-list (.*?)pagination-container')
-    cards = find_all_values(product_list, r'data-id="product".*?product-buy__price-wrap.*?</div></div>')
+    product_list = find_first_value(
+        page, r'class="products-list (.*?)pagination-container')
+    cards = find_all_values(
+        product_list,
+        r'data-id="product".*?product-buy__price-wrap.*?</div></div>')
 
     products = []
     for card in cards:
@@ -47,3 +71,4 @@ if __name__ == "__main__":
               f'{product.description}\t')
 
     print(category_name)
+    to_csv_file(f'{category_name}.csv', products)
