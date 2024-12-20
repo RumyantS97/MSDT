1a49f281e180065a85bb9a34fc0643f6e5312099
Arina564 <arina_zotova_04@mail.ru>
2024-12-20 13:33:14 +0400

    names and del comments

--- msdt-1/Train.py
+++ msdt-1/Train.py
@@ -2,7 +2,7 @@ import logging
 import time
 import os
 
-import tensorflow as tf 
+import tensorflow as tf
 
 from TG_Model import TGModel
 from Process_Poetry import Process_Poetry
@@ -11,33 +11,34 @@ from Config import Config
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 Config.data_path = DIR + '/data/Poem.pkl'
 Config.model_path = DIR + '/model/train'
 
+
 checkpoint_path = DIR + '/model/checkpoint'
-os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+
 data = Process_Poetry(Config)
-x_data,y_data,y_mask,vocab= data.run()
-
-
+x_data, y_data, y_mask, vocab = data.run()
 
 
 with tf.Graph().as_default():
     print("Building Model")
     start = time.time()
-    model = TGModel(Config,'train')
+    model = TGModel(Config, 'train')
     tensors = model.build()
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver(tf.global_variables())
     gpu_options = tf.GPUOptions(allow_growth=True)
-    with tf.Session(config= tf.ConfigProto(gpu_options = gpu_options)) as sess:
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
         sess.run(init)
         if os.path.exists(checkpoint_path):
-            checkpoint = tf.train.latest_checkpoint(DIR + '/model','checkpoint')
+            checkpoint = tf.train.latest_checkpoint(
+                DIR + '/model', 'checkpoint')
             saver.restore(sess, checkpoint)
-        
-        #sess.run(model.global_steps)
-        model.fit(sess,saver,x_data,y_data,y_mask,vocab)
+
+        model.fit(sess, saver, x_data, y_data, y_mask, vocab)
--- msdt-1/poem_generator.py
+++ msdt-1/poem_generator.py
@@ -1,8 +1,8 @@
 import os
 import re
 
-import tensorflow as tf 
-import numpy as np 
+import tensorflow as tf
+import numpy as np
 import pickle as pkl
 
 from TG_Model import TGModel
@@ -10,26 +10,24 @@ from Process_Poetry import Process_Poetry
 from Config import Config
 
 
-class generate():
+class Generate():
 
-    def __init__(self,config,vocab):
+    def __init__(self, config, vocab):
         self.config = Config
         self.vocab = vocab
 
+    def generate_text(self, correct=True):
 
-    
-    def generate_text(self,correct = True):
-        
-        model = TGModel(self.config , 'prediction')
+        model = TGModel(self.config, 'prediction')
         tensors = model.build()
-
 
         init = tf.global_variables_initializer()
         saver = tf.train.Saver(tf.global_variables())
         with tf.Session() as sess:
             sess.run(init)
-            checkpoint = tf.train.latest_checkpoint(DIR + '/model/','checkpoint')
-            saver.restore(sess , checkpoint)
+            checkpoint = tf.train.latest_checkpoint(
+                DIR + '/model/', 'checkpoint')
+            saver.restore(sess, checkpoint)
 
             while True:
                 print('开始生成文本(需要模型),请输入开始词,或quit以退出')
@@ -41,7 +39,7 @@ class generate():
                     words = list(vocab.keys())
                     for i in ['。', '？', '！', 'e']:
                         words.remove(i)
-                    start_word = np.random.choice(words,1)
+                    start_word = np.random.choice(words, 1)
                 try:
                     print('Start!')
                     input_index = []
@@ -52,17 +50,19 @@ class generate():
 
                     punctuation = [vocab['，'], vocab['。'], vocab['？']]
                     punctuation_index = len(start_word)
-                    while index_next not in [0 , vocab['e']] :
+                    while index_next not in [0, vocab['e']]:
                         input_index.append(index_next)
-                        feed = {model.input_placeholder : np.array([input_index])}
-                        y_predict , last_state = sess.run([tensors['prediction'],tensors['last_state']],
-                                                            feed_dict = feed)
+                        feed = {model.input_placeholder: np.array(
+                            [input_index])}
+                        y_predict, last_state = sess.run([tensors['prediction'], tensors['last_state']],
+                                                         feed_dict=feed)
 
                         y_predict = y_predict[-1]
-                        y_predict = {num : i for num , i in enumerate(y_predict)}
-                        index_max = sorted(y_predict , reverse = True , key = lambda x: y_predict[x])[:10]
+                        y_predict = {num: i for num, i in enumerate(y_predict)}
+                        index_max = sorted(
+                            y_predict, reverse=True, key=lambda x: y_predict[x])[:10]
                         index_next = np.random.choice(index_max)
-                        if index_next in [0 , vocab['e']] and len(input_index) < 25:
+                        if index_next in [0, vocab['e']] and len(input_index) < 25:
                             index_next = np.random.choice(index_max)
 
                         punctuation_index += 1
@@ -80,55 +80,33 @@ class generate():
 
                         if len(input_index) > self.config.maxlen:
                             break
-                    int2voc = {self.vocab[word] : word for word in self.vocab}
+                    int2voc = {self.vocab[word]: word for word in self.vocab}
                     text = [int2voc[i] for i in input_index]
                     text = ''.join(text)
 
                 except Exception as e:
-                        print(e)
-                        text = '不能识别%s' % start_word
+                    print(e)
+                    text = '不能识别%s' % start_word
 
                 finally:
-                        text_list = re.findall(pattern='[^。？！]*[。？！]', string=text)
-                        print('作诗完成')
-                        for i in text_list:
-                            print(i)
+                    text_list = re.findall(pattern='[^。？！]*[。？！]', string=text)
+                    print('作诗完成')
+                    for i in text_list:
+                        print(i)
 
-                        print('\nXD\n')
+                    print('\nXD\n')
+
 
 if __name__ == "__main__":
 
-
     DIR = os.path.dirname(os.path.abspath(__file__))
-    os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     Config.data_path = DIR + '/data/Tang_Poetry.pkl'
     Config.model_path = DIR + '/model/train'
 
-    '''
-    data = Process_Poetry(Config)
-    _,_,_,vocab= data.run()
-    with open('data/vocab.pkl','wb')as f:
-        pkl.dump(vocab ,f)
-    '''
-    with open('data/vocab.pkl','rb')as f:
+    with open('data/vocab.pkl', 'rb')as f:
         vocab = pkl.load(f)
     Config.vocab_size = len(vocab.keys()) + 1
-    gen = generate(Config,vocab)
+    gen = Generate(Config, vocab)
 
     gen.generate_text()
-
-'''
-天子龙城去复平，东方南去几何同。
-青山不识三峰里，今去春生在水空。
-
-金陵水边日日暮，
-月华空望云云间，
-行处时处无穷恨。
-一片灯头满窗中？
-
-金陵风吹雪叶中，
-风烟袅杏风前起，
-玉盘帘里空中醉。
-秋光如落酒头回？
-
-'''
