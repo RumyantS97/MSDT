7a5c26358cdfb12881db5a8ecb6b26bfffcff9d6
Leonid <lenyapereponov1@gmail.com>
2024-12-21 00:54:55 +0400

    adding asyncio

--- msdt-7/lr7.py
+++ msdt-7/lr7.py
@@ -1,59 +1,54 @@
-import requests
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup as BS
-
 
 PY_PAGE = 'https://www.python.org'
 
-
-def get_page(url):
-    page_html = requests.get(url).text
-    return page_html
-
+async def get_page(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
 
 def parse_upcoming_events(page_html):
     bs = BS(page_html, 'html.parser')
     event_block = bs.find(text='Upcoming Events')
     event_list = event_block.parent.parent.find(class_='menu').find_all('li')
     event_links = [event.a['href'] for event in event_list]
-    events_info = get_events_info(event_links)
-    return events_info
+    return event_links
 
-
-def get_events_info(links):
-    events_info = []
-    for link in links:
-        event_info = {}
-        event_page = requests.get('{main_url}{event_link}'.format(
-            main_url=PY_PAGE,
-            event_link=link,
-        )).text
+async def get_event_info(session, link):
+    event_info = {}
+    async with session.get(f'{PY_PAGE}{link}') as response:
+        event_page = await response.text()
         bs = BS(event_page, 'html.parser')
 
         title = bs.find(class_='single-event-title')
-        event_info['title'] = title.string.strip()
+        event_info['title'] = title.string.strip() if title else 'No title'
 
         location = bs.find(class_='single-event-location')
-        event_info['location'] = location.string.strip()
+        event_info['location'] = location.string.strip() if location else 'No location'
 
         date = bs.find(class_='single-event-date')
-        event_info['date'] = ' '.join(date.get_text().split())
+        event_info['date'] = ' '.join(date.get_text().split()) if date else 'No date'
 
-        events_info.append(event_info)
+    return event_info
 
+async def get_events_info(links):
+    events_info = []
+    async with aiohttp.ClientSession() as session:
+        tasks = [get_event_info(session, link) for link in links]
+        events_info = await asyncio.gather(*tasks)
     return events_info
-
 
 def pprint_events(events):
     for event in events:
-        print('-'*10)
-        print('Title: {title}\nLocation: {loc}\nDate: {date}'.format(
-            title=event['title'],
-            loc=event['location'],
-            date=event['date'],
-        ))
+        print('-' * 10)
+        print(f"Title: {event['title']}\nLocation: {event['location']}\nDate: {event['date']}")
 
+async def main():
+    page = await get_page(PY_PAGE)
+    event_links = parse_upcoming_events(page)
+    events_info = await get_events_info(event_links)
+    pprint_events(events_info)
 
-if __name__ == '__main__':
-    page = get_page(PY_PAGE)
-    upcoming_events = parse_upcoming_events(page)
-    pprint_events(upcoming_events)
+asyncio.run(main())
