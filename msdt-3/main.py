bc945b5a342b236cbcedac15c4b200751c870cd0
Мария Фридман <fridmanmariya14@yandex.ru>
2024-12-21 19:46:26 +0400

    Код приведен к PEP8

--- msdt-3/main.py
+++ msdt-3/main.py
@@ -5,20 +5,20 @@ import pandas as pd
 import checksum as check
 
 PATTERNS = {
-    "email" : r"^\w+(\.\w+)*@\w+(\.\w+)+$", ###
-    "height" : r"^[1-2]\.\d{2}$", ###
-    "snils" : r"^\d{11}$", ###
-    "passport" : r"^(\d{2}\s){2}\d{6}$", ###
-    "occupation" : r"^[а-яА-Яa-zA-Z\s-]+$", ###
-    "longitude" : r"^-?(180|1[0-7]\d|\d{1,2})(\.\d+)?$", ###
-    "hex_color" : r"^#([a-f0-9]{6}|[a-f0-9]{3})$", ###
-    "issn" : r"^\d{4}-\d{4}$", ###
-    "locale_code" : r"^[a-z]{1,3}(-[a-z]+)?(-[a-z]{2})?$", ###
-    "time" : r"^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)\.(\d{1,6})$", ###
+    "email"      : r"^\w+(\.\w+)*@\w+(\.\w+)+$",
+    "height"     : r"^[1-2]\.\d{2}$",
+    "snils"      : r"^\d{11}$",
+    "passport"   : r"^(\d{2}\s){2}\d{6}$",
+    "occupation" : r"^[а-яА-Яa-zA-Z\s-]+$",
+    "longitude"  : r"^-?(180|1[0-7]\d|\d{1,2})(\.\d+)?$",
+    "hex_color"  : r"^#([a-f0-9]{6}|[a-f0-9]{3})$",
+    "issn"       : r"^\d{4}-\d{4}$",
+    "locale_code": r"^[a-z]{1,3}(-[a-z]+)?(-[a-z]{2})?$",
+    "time"       : r"^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)\.(\d{1,6})$"
 }
 
 
-def check_row(row : pd.Series) -> bool:
+def check_row(row: pd.Series) -> bool:
     for name_column, value in zip(PATTERNS.keys(), row):
         pattern = PATTERNS[name_column]
         if not re.search(pattern, str(value)):
@@ -26,14 +26,14 @@ def check_row(row : pd.Series) -> bool:
     return True
 
 
-def open_csv(path : str) -> pd.DataFrame:
+def open_csv(path: str) -> pd.DataFrame:
     data = pd.read_csv(path, encoding="utf-16", sep=";")
     return data
 
 
-def get_invalid_index(path : str) -> list:
+def get_invalid_index(path: str) -> list:
     data = open_csv(path)
-    invalid_index_list = list()
+    invalid_index_list = []
     for index, row in data.iterrows():
         if not check_row(row):
             invalid_index_list.append(index)
