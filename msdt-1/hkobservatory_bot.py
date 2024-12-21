628d9b1b956b17d967df8c1119d017d3362f0c32
Ronaldovich163RUS <anatoliy.kozlov.04@mail.ru>
2024-12-21 14:51:57 +0400

    строки меньше 79 символов, удалены комментарии, пустые строки между частями кода

--- msdt-1/hkobservatory_bot.py
+++ msdt-1/hkobservatory_bot.py
@@ -15,7 +15,6 @@ dispatcher = updater.dispatcher
 job_queue = updater.job_queue
 
 
-# Checks if feeds have updated by comparing saved feed dates to new feed
 def check_feed_update():
     try:
         with open(feed) as f:
@@ -26,19 +25,33 @@ def check_feed_update():
 
         if current_en_update:
             current_en = feeds["current"][0]
-            if current_en["entries"][0]["published"] != current_en_update.entries[0].published:
+            if (
+                current_en["entries"][0]["published"]
+                != current_en_update.entries[0].published
+            ):
                 current_trad_update = feedparser.parse(weather3)
                 current_simp_update = feedparser.parse(weather4)
-                current_update = [current_en_update, current_trad_update, current_simp_update]
+                current_update = [
+                    current_en_update,
+                    current_trad_update,
+                    current_simp_update,
+                ]
                 updates["current"] = current_update
                 feeds["current"] = current_update
 
         if warning_en_update:
             warning_en = feeds["warning"][0]
-            if warning_en["entries"][0]["published"] != warning_en_update.entries[0].published:
+            if (
+                warning_en["entries"][0]["published"]
+                != warning_en_update.entries[0].published
+            ):
                 warning_trad_update = feedparser.parse(weather5)
                 warning_simp_update = feedparser.parse(weather6)
-                warning_update = [warning_en_update, warning_trad_update, warning_simp_update]
+                warning_update = [
+                    warning_en_update,
+                    warning_trad_update,
+                    warning_simp_update,
+                ]
                 updates["warning"] = warning_update
                 feeds["warning"] = warning_update
 
@@ -54,7 +67,7 @@ def check_feed_update():
         warning = [warning_en, warning_trad, warning_simp]
 
         with open(feed, "w") as f:
-            updates = {"current":current, "warning":warning}
+            updates = {"current": current, "warning": warning}
             feeds = updates
             json.dump(updates, f)
 
@@ -64,24 +77,24 @@ def check_feed_update():
     return updates
 
 
-# Returns language preferences for all users
 def get_user_language():
     try:
         with open(lang) as f:
             user_language = json.load(f)
     except FileNotFoundError:
         user_language = {}
-    return user_language;
+    return user_language
 
 
 def get_topics():
-    topics = ["Current - Current weather information",
-              "Warning - Warnings in force"]
+    topics = [
+        "Current - Current weather information",
+        "Warning - Warnings in force",
+    ]
     topics = "The topics I can tell you about are:\n" + "\n".join(topics)
     return topics
 
 
-# Returns the formatted feed in the user's preferred language
 def get_feed_message(user_id, topic):
     check_feed_update()
     user_language = get_user_language()
@@ -121,11 +134,11 @@ def get_feed_message(user_id, topic):
 
 
 def start(bot, update):
-    message = "Hi, I'm HKObservatoryBot! Type @hkobservatory_bot to see what I can do!"
+    message = "Hi, I'm HKObservatoryBot! \
+        Type @hkobservatory_bot to see what I can do!"
     bot.sendMessage(chat_id=update.message.chat_id, text=message)
 
 
-# Handles inline queries from user
 def inline_query(bot, update):
     query = update.inline_query.query
     results = []
@@ -138,9 +151,13 @@ def inline_query(bot, update):
                 id="commands",
                 title="Commands",
                 input_message_content=telegram.InputTextMessageContent(
-                    ("Type @hkobservatory_bot + one of the following:\n"
-                    "topics;\ntellme + topic;\nsubscribe + topic;\nunsubscribe + topic;\nenglish;\n繁體中文;\n简体中文;")),
-                description="List of available commands"
+                    (
+                        "Type @hkobservatory_bot + one of the following:\n"
+                        "topics;\ntellme + topic;\nsubscribe + topic\
+                            ;\nunsubscribe + topic;\nenglish;\n繁體中文;\n简体中文;"
+                    )
+                ),
+                description="List of available commands",
             )
         )
     else:
@@ -149,8 +166,10 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="topics",
                     title="Topics",
-                    input_message_content=telegram.InputTextMessageContent(get_topics()),
-                    description="List of available topics"
+                    input_message_content=telegram.InputTextMessageContent(
+                        get_topics()
+                    ),
+                    description="List of available topics",
                 )
             )
         if query.lower() in "tellme current":
@@ -158,8 +177,10 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="tellme_current",
                     title="Current Weather",
-                    input_message_content=telegram.InputTextMessageContent(get_feed_message(user_id, "current")),
-                    description="Current weather from the HK Observatory"
+                    input_message_content=telegram.InputTextMessageContent(
+                        get_feed_message(user_id, "current")
+                    ),
+                    description="Current weather from the HK Observatory",
                 )
             )
         if query.lower() in "tellme warning":
@@ -167,8 +188,10 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="tellme_warning",
                     title="Warning",
-                    input_message_content=telegram.InputTextMessageContent(get_feed_message(user_id, "warning")),
-                    description="Warnings in force"
+                    input_message_content=telegram.InputTextMessageContent(
+                        get_feed_message(user_id, "warning")
+                    ),
+                    description="Warnings in force",
                 )
             )
         if query.lower() in "subscribe current":
@@ -176,8 +199,10 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="sub_current",
                     title="Subscribe Current",
-                    input_message_content=telegram.InputTextMessageContent(first_name + " has subscribed to: Current"),
-                    description="Subscribe to current to receive updates"
+                    input_message_content=telegram.InputTextMessageContent(
+                        first_name + " has subscribed to: Current"
+                    ),
+                    description="Subscribe to current to receive updates",
                 )
             )
         if query.lower() in "subscribe warning":
@@ -185,8 +210,10 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="sub_warning",
                     title="Subscribe Warning",
-                    input_message_content=telegram.InputTextMessageContent(first_name + " has subscribed to: Warning"),
-                    description="Subscribe to warning to receive updates"
+                    input_message_content=telegram.InputTextMessageContent(
+                        first_name + " has subscribed to: Warning"
+                    ),
+                    description="Subscribe to warning to receive updates",
                 )
             )
         if query.lower() in "unsubscribe current":
@@ -194,8 +221,11 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="unsub_current",
                     title="Unsubscribe Current",
-                    input_message_content=telegram.InputTextMessageContent(first_name + " has unsubscribed from: Current"),
-                    description="Unsubscribe from current to stop receiving updates"
+                    input_message_content=telegram.InputTextMessageContent(
+                        first_name + " has unsubscribed from: Current"
+                    ),
+                    description="Unsubscribe from current\
+                          to stop receiving updates",
                 )
             )
         if query.lower() in "unsubscribe warning":
@@ -203,8 +233,11 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="unsub_warning",
                     title="Unsubscribe Warning",
-                    input_message_content=telegram.InputTextMessageContent(first_name + " has unsubscribed from: Warning"),
-                    description="Unsubscribe from warning to stop receiving updates"
+                    input_message_content=telegram.InputTextMessageContent(
+                        first_name + " has unsubscribed from: Warning"
+                    ),
+                    description="Unsubscribe from warning\
+                          to stop receiving updates",
                 )
             )
         if query.lower() in "english":
@@ -212,8 +245,10 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="lang_english",
                     title="English",
-                    input_message_content=telegram.InputTextMessageContent(first_name + "\'s language changed to English"),
-                    description="Select English as topic information language"
+                    input_message_content=telegram.InputTextMessageContent(
+                        first_name + "'s language changed to English"
+                    ),
+                    description="Select English as topic information language",
                 )
             )
         if query in "繁體中文":
@@ -221,8 +256,10 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="lang_traditional",
                     title="繁體中文",
-                    input_message_content=telegram.InputTextMessageContent(first_name + "\'s language changed to 繁體中文"),
-                    description="Select 繁體中文 as topic information language"
+                    input_message_content=telegram.InputTextMessageContent(
+                        first_name + "'s language changed to 繁體中文"
+                    ),
+                    description="Select 繁體中文 as topic information language",
                 )
             )
         if query in "简体中文":
@@ -230,14 +267,15 @@ def inline_query(bot, update):
                 telegram.InlineQueryResultArticle(
                     id="lang_simplified",
                     title="简体中文",
-                    input_message_content=telegram.InputTextMessageContent(first_name + "\'s language changed to 简体中文"),
-                    description="Select 简体中文 as topic information language"
+                    input_message_content=telegram.InputTextMessageContent(
+                        first_name + "'s language changed to 简体中文"
+                    ),
+                    description="Select 简体中文 as topic information language",
                 )
             )
     bot.answerInlineQuery(update.inline_query.id, results, cache_time=0)
 
 
-# Saves language preferences and subscriptions
 def inline_result(bot, update):
     result_id = update.chosen_inline_result.result_id
     user_id = str(update.chosen_inline_result.from_user.id)
@@ -278,7 +316,6 @@ def inline_result(bot, update):
             json.dump(subscribers, f)
 
 
-# Sends updates to subscribed users
 def send_update(bot, job):
     try:
         with open(subs) as f:
@@ -302,7 +339,6 @@ def send_update(bot, job):
                     pass
 
 
-# Bot will check for updates every hour
 job_queue.put(telegram.ext.Job(send_update, 3600.0))
 
 # Handlers for commands, inline queries and results
